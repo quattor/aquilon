f06f67ca5da49e5274c121e9c2e2605cb82adc55
Scott DAquila <daqscott@ms.com>
2008-06-06 14:44:13 -0500

    creation_date is NON_NULL
    [git-p4: depot-paths = "//eai/aquilon/aqd/": change = 642113]

--- 1.1.1/src/migrate/schema.py
+++ 1.1.1/src/migrate/schema.py
@@ -4,7 +4,8 @@ def get_id_col(name):
     return Column('id', Integer, Sequence('%s_seq'%(name)),primary_key=True)
 
 def get_date_col():
-    return Column('creation_date', DateTime, default = datetime.now)
+    return Column('creation_date', DateTime,
+                  default = datetime.now, nullable = False)
 
 def get_comment_col():
     return Column('comments', String(255), nullable = True)
