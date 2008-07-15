2eaf9eccd827b71edc4308e0fa850f8c6cfc6a56
Scott DAquila <daqscott@ms.com>
2008-07-15 15:34:59 -0500

    has its own dependencies for now
    [git-p4: depot-paths = "//eai/aquilon/aqd/": change = 645929]

--- 1.2.1/src/lib/python2.5/aquilon/aqdb/shell.py
+++ 1.2.1/src/lib/python2.5/aquilon/aqdb/shell.py
@@ -1,5 +1,9 @@
 #!/ms/dist/python/PROJ/core/2.5.0/bin/python
+import msversion
 
+msversion.addpkg('sqlalchemy', '0.5beta', 'dev')
+msversion.addpkg('cx_Oracle','4.4-10.2.0.1','dist')
+msversion.addpkg('ipython','0.8.2','dist')
 
 import sys
 import os
@@ -7,7 +11,7 @@ import os
 if __name__ == '__main__':
     DIR = os.path.dirname(os.path.realpath(__file__))
     sys.path.insert(0, os.path.realpath(os.path.join(DIR, '..', '..')))
-    import aquilon.aqdb.depends
+    #import aquilon.aqdb.depends
 
 from aquilon.aqdb.utils.shell import ipshell
 from aquilon.aqdb.db_factory import db_factory, Base
@@ -20,4 +24,3 @@ s = dbf.session()
 
 from aquilon.aqdb.utils.table_admin import *
 ipshell()
-
