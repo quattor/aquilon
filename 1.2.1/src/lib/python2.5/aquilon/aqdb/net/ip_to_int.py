63a352a078b7cd78d10cec2d90750d734a802258
Scott DAquila <daqscott@ms.com>
2008-08-05 15:21:29 -0500

    some very minor cleanup and noticed that we've got a circular import without importing network as n. To be fixed later
    [git-p4: depot-paths = "//eai/aquilon/aqd/": change = 648428]

--- 1.2.1/src/lib/python2.5/aquilon/aqdb/net/ip_to_int.py
+++ 1.2.1/src/lib/python2.5/aquilon/aqdb/net/ip_to_int.py
@@ -23,43 +23,35 @@ if __name__ == '__main__':
     sys.path.insert(0, os.path.realpath(os.path.join(DIR, '..', '..', '..')))
     import aquilon.aqdb.depends
 
+from sqlalchemy import func, select
 from sqlalchemy.sql import and_
 
 from aquilon.exceptions_ import ArgumentError
 
-
+#these are just the conversion routines. Boundary checking to be done in IPV4
 def dq_to_int(dq):
     return struct.unpack('!L', inet_aton(dq))[0]
 
 def int_to_dq(n):
-    #Force the incoming Decimal to a long to prevent odd issues when
-    #struct.pack() tries it...
+    #Force incoming Decimal to a long to prevent odd issues from sruct.pack()
     return inet_ntoa(struct.pack('!L', long(n)))
 
-def test_ip_to_int():
-    t1 = '192.168.1.1'
-    t2 = '144.14.47.54'
-    t3 = '144.203.222.128'
+def test_ip_to_int(i='144.203.222.162'):
+    tmp_int = dq_to_int(i)
+    print '%s --> %d'%(i, tmp_int)
 
-    for i in [t1, t2, t3]:
-        tmp_int = dq_to_int(i)
-        print '%s --> %d'%(i, tmp_int)
+    tmp_dq  = int_to_dq(tmp_int)
+    print '%d --> %s'%(tmp_int, tmp_dq)
 
-        tmp_dq  = int_to_dq(tmp_int)
-        print '%d --> %s'%(tmp_int, tmp_dq)
-
-        assert tmp_dq == i
+    assert tmp_dq == i
 
 def get_net_id_from_ip(s, ip):
     """Requires a session, and will return the Network for a given ip."""
-
     if ip is None:
         return None
 
-    from sqlalchemy import func, select
+    #FIX ME: make this a staticmethod on Network (circular import for now)
     import aquilon.aqdb.net.network as n
-    #makes things a tiny bit more readable to me (daqscott)
-    #don't worry about runtime, this is for test only
 
     Network = n.Network
     network = n.network
@@ -69,11 +61,11 @@ def get_net_id_from_ip(s, ip):
 
     s2 = select([network.c.id], network.c.ip == s1)
 
-    target_id = s.execute(s2).fetchone()
-    if not target_id:
+    row = s.execute(s2).fetchone()
+    if not row:
         raise ArgumentError("Could not determine network for ip %s" % ip)
 
-    return s.query(Network).get(target_id[0])
+    return s.query(Network).get(row[0])
 
 def cidr_to_int(cidr):
     return(0xffffffffL >> (32- cidr)) << (32 - cidr)
