2bbed28166f062f4b161c0eef32394ed6c4edc47
Ben Jones <benjones@maya.(none)>
2008-08-21 14:31:58 +0100

    Show service by server
    This adds a --server flag to show_service, which will show all service instances the server is responsible for.

--- lib/python2.5/aquilon/server/commands/show_service_service.py
+++ lib/python2.5/aquilon/server/commands/show_service_service.py
@@ -1,9 +1,5 @@
 #!/ms/dist/python/PROJ/core/2.5.0/bin/python
 # ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
-# $Header$
-# $Change$
-# $DateTime$
-# $Author$
 # Copyright (C) 2008 Morgan Stanley
 #
 # This module is part of Aquilon
@@ -13,7 +9,9 @@
 from aquilon.server.broker import (format_results, add_transaction, az_check,
                                    BrokerCommand)
 from aquilon.server.dbwrappers.service import get_service
-from aquilon.server.dbwrappers.service_instance import get_service_instance
+from aquilon.server.dbwrappers.system import get_system
+from aquilon.aqdb.svc.service_instance import ServiceInstance
+from aquilon.server.formats.service_instance import ServiceInstanceList
 
 
 class CommandShowServiceService(BrokerCommand):
@@ -23,11 +21,17 @@ class CommandShowServiceService(BrokerCommand):
     @add_transaction
     @az_check
     @format_results
-    def render(self, session, service, instance, **arguments):
+    def render(self, session, service, instance, server, **arguments):
         dbservice = get_service(session, service)
-        if not instance:
+        if not instance and not server:
             return dbservice
-        return get_service_instance(session, dbservice, instance)
+        q = session.query(ServiceInstance).filter_by(service=dbservice)
+        if instance:
+            q = q.filter_by(name=instance)
+        if server:
+            dbsystem = get_system(session, server)
+            q = q.join('servers').filter_by(system=dbsystem)
+        return ServiceInstanceList(q.all())
 
 
 #if __name__=='__main__':
