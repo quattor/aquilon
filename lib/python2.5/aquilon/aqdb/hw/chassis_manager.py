2b41286ec2cb3e0e5fe0feefd77e2746cde30b32
Scott D'Aquila <daqscott@blackcomb.(none)>
2008-09-02 18:49:18 -0400

    new hardware types

---
+++ lib/python2.5/aquilon/aqdb/hw/chassis_manager.py
@@ -0,0 +1,58 @@
+#!/ms/dist/python/PROJ/core/2.5.0/bin/python
+"""The tables/objects/mappings related to hardware in Aquilon. """
+
+from datetime import datetime
+import sys
+import os
+
+if __name__ == '__main__':
+    DIR = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(0, os.path.realpath(os.path.join(DIR, '..', '..', '..')))
+    import aquilon.aqdb.depends
+
+from sqlalchemy import (UniqueConstraint, Table, Column, Integer, DateTime,
+                        Sequence, String, select, ForeignKey, Index)
+
+from sqlalchemy.orm      import relation, deferred, backref
+
+from aquilon.aqdb.column_types.aqstr  import AqStr
+from aquilon.aqdb.hw.hardware_entity  import HardwareEntity
+
+class ChassisManager(HardwareEntity):
+    __tablename__ = 'chassis_manager'
+    __mapper_args__ = {'polymorphic_identity' : 'chassis_manager'}
+
+    hardware_entity_id = Column(Integer,
+                                ForeignKey(HardwareEntity.c.id,
+                                           name = 'chassis_manager_hw_ent_fk',
+                                           ondelete = 'CASCADE'),
+
+                                           primary_key = True)
+
+    name = Column('name', AqStr(64), nullable = False)
+    #TODO: move name back to hw_entity? I *know* this isn't done yet
+
+    #TODO: synonym in location lest we break things
+    #location = relation(Location, uselist = False)
+    #model
+    hardware_entity = relation(HardwareEntity,
+                               uselist = False,
+                               backref = 'chassis_manager')
+
+
+chassis_manager = ChassisManager.__table__
+chassis_manager.primary_key.name = 'chassis_manager_pk'
+
+chassis_manager.append_constraint(
+    UniqueConstraint('name',name = 'chassis_manager_name_uk')
+)
+
+table = chassis_manager
+
+#TODO: OA interface type or just public iface???
+
+# Copyright (C) 2008 Morgan Stanley
+# This module is part of Aquilon
+
+# ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
+
---
+++ lib/python2.5/aquilon/aqdb/hw/tor_switch.py
@@ -0,0 +1,58 @@
+#!/ms/dist/python/PROJ/core/2.5.0/bin/python
+"""The tables/objects/mappings related to hardware in Aquilon. """
+
+from datetime import datetime
+import sys
+import os
+
+if __name__ == '__main__':
+    DIR = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(0, os.path.realpath(os.path.join(DIR, '..', '..', '..')))
+    import aquilon.aqdb.depends
+
+from sqlalchemy import (UniqueConstraint, Table, Column, Integer, DateTime,
+                        Sequence, String, select, ForeignKey, Index)
+
+from sqlalchemy.orm  import relation, deferred, backref
+
+from aquilon.aqdb.db_factory          import Base
+from aquilon.aqdb.column_types.aqstr  import AqStr
+from aquilon.aqdb.hw.hardware_entity  import HardwareEntity
+
+#TODO: use selection of the tor_switch specs to dynamically populate default
+#     values for all of the attrs where its possible
+
+class TorSwitch(HardwareEntity):
+    __tablename__ = 'tor_switch'
+    __mapper_args__ = {'polymorphic_identity' : 'tor_switch'}
+
+    hardware_entity_id = Column(Integer,
+                                ForeignKey(HardwareEntity.c.id,
+                                           name = 'tor_switch_hw_ent_fk',
+                                           ondelete = 'CASCADE'),
+                                           primary_key = True)
+    
+    #TODO: this is not done: fk to a_name?
+    # also proxy attributes to parent
+    name = Column('name', AqStr(64), nullable = False)
+
+    hardware_entity = relation(HardwareEntity, uselist = False,
+                               backref = 'tor_switch', passive_deletes = True)
+
+
+tor_switch = TorSwitch.__table__
+tor_switch.primary_key.name = 'tor_switch_pk'
+
+tor_switch.append_constraint(
+    UniqueConstraint('name',name = 'tor_switch_name_uk')
+)
+
+table = tor_switch
+
+#TODO: make tor_switch --like [other tor_switch] + overrides
+
+# Copyright (C) 2008 Morgan Stanley
+# This module is part of Aquilon
+
+# ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
+
