cf6a24ac549f68508441721ffc5c89d5e1ed6a5a
Scott D'Aquila <daqscott@blackcomb.(none)>
2008-09-02 15:00:12 -0400

    latest stuff

--- lib/python2.5/aquilon/aqdb/sy/__init__.py
+++ lib/python2.5/aquilon/aqdb/sy/__init__.py
@@ -1,10 +1,21 @@
-__all__ = (
-            'system',
-            'host',
-            'domain',
-            'build_item',
-            'host_list',
-            'host_list_item',
-            'system_list',
-            'system_list_item'
-          )
+""" The auth package consists of all objects and tables
+    for authentication, authorization and audit """
+
+import os
+import sys
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.realpath(os.path.join(DIR, '..', '..', '..')))
+
+from aquilon.aqdb.depends import get_files
+
+__all__ = get_files(DIR)
+
+del os
+del sys
+
+# Copyright (C) 2008 Morgan Stanley
+# This module is part of Aquilon
+
+# ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
+
