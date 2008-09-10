117e054a2f15a46ca0fdbf6dfe14a692451a6fdc
Nick Williams <Nick.Williams@morganstanley.com>
2008-09-10 13:03:56 +0100

    removing old XML files from the compile process to avoid stale profiles The old XML profiles are being left behind in multiple build domains and the makefile does a blind rsync. Therefore, you can end up copying across old profiles.

--- lib/python2.5/aquilon/server/commands/manage.py
+++ lib/python2.5/aquilon/server/commands/manage.py
@@ -10,10 +10,12 @@
 """Contains the logic for `aq manage`."""
 
 
+import os
 from aquilon.server.broker import (format_results, add_transaction, az_check,
                                    BrokerCommand)
 from aquilon.server.dbwrappers.domain import verify_domain
 from aquilon.server.dbwrappers.host import hostname_to_host
+from aquilon.server.templates.host import PlenaryHost
 
 
 class CommandManage(BrokerCommand):
@@ -27,8 +29,27 @@ class CommandManage(BrokerCommand):
         dbdomain = verify_domain(session, domain,
                 self.config.get("broker", "servername"))
         dbhost = hostname_to_host(session, hostname)
+
+        # Clean up any old files in the old domain
+        # Note, that these files may not exist if we've never compiled
+        # in the old domain, so we just try the lot.
+        # We don't create a new plenary host in the new domain - that'll
+        # be done lazily when someone next tries to compile the new domain.
+        try:
+            builddir = os.path.join(self.config.get("broker", "builddir"), "domains", dbhost.domain.name, "profiles")
+            plenary = PlenaryHost(dbhost)
+            plenary.remove(builddir)
+            plenary = None
+            file = os.path.join(qdir, "build", "xml", domain, fqdn+".xml")
+            os.remove(file)
+            file = os.path.join(qdir, "build", "xml", domain, fqdn+".xml.dep")
+            os.remove(file)
+        finally:
+            pass
+
         dbhost.domain = dbdomain
         session.save_or_update(dbhost)
+
         return
 
 
