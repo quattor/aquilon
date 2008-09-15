467b423993e6c4fa816fbde70c9017098442caf0
Wesley Hertlein <wesleyhe@ms.com>
2008-09-15 11:12:36 -0400

    Added authorization for aquilon hosts to modify their own resources.
    For now, this is a primitive check, and done in two parts.  The first
    squashes all incoming principals that look like
    host/hostname_in_aqdb.ms.com@is1.morgan to aquilonhost@is1.morgan.
    The second part ensures that this principal is allowed to take action
    on any resources that start with /host/hostname_in_aqdb.ms.com/.

--- lib/python2.5/aquilon/server/dbwrappers/user_principal.py
+++ lib/python2.5/aquilon/server/dbwrappers/user_principal.py
@@ -1,9 +1,5 @@
 #!/ms/dist/python/PROJ/core/2.5.0/bin/python
 # ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
-# $Header$
-# $Change$
-# $DateTime$
-# $Author$
 # Copyright (C) 2008 Morgan Stanley
 #
 # This module is part of Aquilon
@@ -17,9 +13,11 @@ from aquilon.exceptions_ import ArgumentError
 from aquilon.aqdb.auth.user_principal import UserPrincipal
 from aquilon.aqdb.auth.realm import Realm
 from aquilon.aqdb.auth.role import Role
+from aquilon.server.dbwrappers.host import hostname_to_host
 
 
 principal_re = re.compile(r'^(.*)@([^@]+)$')
+host_re = re.compile(r'^host/(.*)@([^@]+)$')
 
 def get_or_create_user_principal(session, user,
         createuser=True, createrealm=True):
@@ -31,6 +29,13 @@ def get_or_create_user_principal(session, user,
         raise ArgumentError("Could not parse user principal '%s'" % user)
     realm = m.group(2)
     user = m.group(1)
+    m = host_re.match(principal)
+    if m:
+        user = 'aquilonhost'
+        hostname = m.group(1)
+        # Don't actually need the host... just need to verify that it's
+        # in aqdb.
+        dbhost = hostname_to_host(session, hostname)
     dbrealm = session.query(Realm).filter_by(name=realm).first()
     dbnobody = session.query(Role).filter_by(name='nobody').first()
     if not dbrealm:
@@ -40,7 +45,7 @@ def get_or_create_user_principal(session, user,
         log.msg("Realm %s did not exist, creating..." % realm)
         dbrealm = Realm(name=realm)
         session.save(dbrealm)
-        log.msg("Creating user %s..." % principal)
+        log.msg("Creating user %s@%s..." % (user, realm))
         dbuser = UserPrincipal(name=user, realm=dbrealm, role=dbnobody)
         session.save(dbuser)
         return dbuser
