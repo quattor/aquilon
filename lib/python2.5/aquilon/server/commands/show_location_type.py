af6db834f2c77f0c3d6d37ba1956cb1d490e7d88
Wesley Hertlein <wesleyhe@ms.com>
2008-11-17 10:24:07 -0500

    show_location: partial speedup for queries.
    Addresses-Issue: Jira/AQUILONAQD-169
    Request an immediate outer join during the Location query, reducing
    round trips to the database.  This is only a partial speedup - we would
    get a much greater improvement by being able to query for all parents
    in a location heirarchy in a single shot.
    
    Timing tests showed that show_location --type=building went from
    7 seconds to 5 seconds with this fix.  Commenting out the display of
    location parents brings it down to 2 seconds.

--- lib/python2.5/aquilon/server/commands/show_location_type.py
+++ lib/python2.5/aquilon/server/commands/show_location_type.py
@@ -1,9 +1,5 @@
 #!/ms/dist/python/PROJ/core/2.5.0/bin/python
 # ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
-# $Header$
-# $Change$
-# $DateTime$
-# $Author$
 # Copyright (C) 2008 Morgan Stanley
 #
 # This module is part of Aquilon
@@ -27,6 +23,8 @@ class CommandShowLocationType(BrokerCommand):
     @format_results
     def render(self, session, type, name, **arguments):
         query = session.query(Location)
+        query = query.with_polymorphic(
+            Location.__mapper__.polymorphic_map.values())
         if type:
             query = query.filter_by(location_type=type)
         if name:
