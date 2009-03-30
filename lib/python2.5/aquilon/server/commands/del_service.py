dc60888f559579c193bf64e93c3e06c81c626240
Wesley Hertlein <wesleyhe@ms.com>
2009-03-30 15:59:04 -0400

    del_service: Miscellaneous fixes for checks and plenary writing.
    Make sure database action and flush happens before attempting to
    write any plenary templates.
    
    Test for servers that back an instance before attempting to
    delete the instance.

--- lib/python2.5/aquilon/server/commands/del_service.py
+++ lib/python2.5/aquilon/server/commands/del_service.py
@@ -30,6 +30,9 @@ class CommandDelService(BrokerCommand):
             if dbservice.instances:
                 raise ArgumentError("Cannot remove service with instances defined.")
 
+            session.delete(dbservice)
+            session.flush()
+
             plenary_info = PlenaryService(dbservice)
             plenary_info.remove()
 
@@ -39,7 +42,6 @@ class CommandDelService(BrokerCommand):
             plenary_info = PlenaryServiceServerDefault(dbservice)
             plenary_info.remove()
 
-            session.delete(dbservice)
             return
         dbsi = session.query(ServiceInstance).filter_by(
                 name=instance, service=dbservice).first()
@@ -47,6 +49,15 @@ class CommandDelService(BrokerCommand):
         if dbsi:
             if dbsi.client_count > 0:
                 raise ArgumentError("instance has clients and cannot be deleted.")
+            if dbsi.servers:
+                raise ArgumentError("instance is still being provided by servers.")
+
+            # Check the service map and remove any mappings
+            for dbmap in session.query(ServiceMap).filter_by(service_instance=dbsi).all():
+                session.delete(dbmap)
+
+            session.delete(dbsi)
+            session.flush()
 
             plenary_info = PlenaryServiceInstance(dbservice, dbsi)
             plenary_info.remove()
@@ -60,14 +71,6 @@ class CommandDelService(BrokerCommand):
             plenary_info = PlenaryServiceInstanceServerDefault(dbservice, dbsi)
             plenary_info.remove()
 
-            # Check the service map and remove any mappings
-            for dbmap in session.query(ServiceMap).filter_by(service_instance=dbsi).all():
-                session.delete(dbmap)
-
-            session.delete(dbsi)
-            session.flush()
-            session.refresh(dbservice)
-            
         # FIXME: Cascade to relevant objects...
         return
 
