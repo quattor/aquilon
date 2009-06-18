c3fab4c82390c8d4584bb9e9025725e90f6b86bf
Wesley Hertlein <wesleyhe@ms.com>
2009-06-18 16:54:26 -0400

    Added a patch for AssociationProxy to better handle null scalar values.
    I needed this to stay sane.  I thought it might also be the root cause
    of the fun issue that requires autoflush to be False in the aqdb unit
    tests but alas that issue remains unsolved.

--- lib/python2.5/aquilon/aqdb/model/base.py
+++ lib/python2.5/aquilon/aqdb/model/base.py
@@ -26,9 +26,12 @@
 # SOFTWARE MAY BE REDISTRIBUTED TO OTHERS ONLY BY EFFECTIVELY USING
 # THIS OR ANOTHER EQUIVALENT DISCLAIMER AS WELL AS ANY OTHER LICENSE
 # TERMS THAT MAY APPLY.
+import weakref
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import UniqueConstraint, PrimaryKeyConstraint
 from sqlalchemy import Integer
+from sqlalchemy.ext.associationproxy import AssociationProxy
 
 from aquilon.utils import monkeypatch
 from aquilon.exceptions_ import InternalError
@@ -137,3 +140,43 @@ def __init__(self, **kw):
             msg = "%r is an invalid argument for %s" %(k, type(self).__name__)
             raise TypeError(msg)
         setattr(self, k, kw[k])
+
+# WAY too much magic in AssociationProxy.  This bug and proposed patch is
+# listed in the second half of this message:
+# http://groups.google.com/group/sqlalchemy-devel/browse_thread/thread/973f4104007f6964/9a001201b3179c58
+# Basically, scalar assocation proxies are much more annoying without this
+# patch.  Accessing a null attribute normally returns None.  However, the AP
+# tries to proxy through the None.  This raises an exception when the AP
+# (in a scalar context) should just itself return None.
+@monkeypatch(AssociationProxy)
+def __get__(self, obj, class_):
+    if self.owning_class is None:
+        self.owning_class = class_ and class_ or type(obj)
+    if obj is None:
+        return self
+    elif self.scalar is None:
+        self.scalar = self._target_is_scalar()
+        if self.scalar:
+            self._initialize_scalar_accessors()
+
+    if self.scalar:
+        # Original line from 0.5.4
+        #return self._scalar_get(getattr(obj, self.target_collection))
+        target = getattr(obj, self.target_collection)
+        if target is None:
+            return None
+        else:
+            return self._scalar_get(target)
+    else:
+        try:
+            # If the owning instance is reborn (orm session resurrect,
+            # etc.), refresh the proxy cache.
+            creator_id, proxy = getattr(obj, self.key)
+            if id(obj) == creator_id:
+                return proxy
+        except AttributeError:
+            pass
+        proxy = self._new(self._lazy_collection(weakref.ref(obj)))
+        setattr(obj, self.key, (id(obj), proxy))
+        return proxy
+
