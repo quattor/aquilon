d37453c6cfd8ad2f03eae61aa13ab4168a9d837b
Scott D'Aquila <daqscott@oy608c1n2.ms.com>
2009-06-25 14:51:23 +0100

    removes kerberos libraries and simplifies the code
    It only affects me, but my kerberos privs seem to have
    been revoked from dsdb without warning. This is a clean
    up that I require.

--- lib/python2.5/aquilon/aqdb/dsdb/dsdb.py
+++ lib/python2.5/aquilon/aqdb/dsdb/dsdb.py
@@ -33,9 +33,8 @@ import os
 import sys
 import ms.version
 from copy import copy
-#from   ConfigParser import SafeConfigParser
 
-ms.version.addpkg('sybase', '0.38-py25', 'dist')
+ms.version.addpkg('sybase', '0.39-15.0.0.17')
 import Sybase
 
 from dispatch_table import dispatch_tbl as dt
@@ -44,10 +43,7 @@ class DsdbConnection(object):
     """ Wraps connections to DSDB """
     def __init__(self, fake=False, *args, **kw):
 
-#10/23/08 Heavy turonover blackout: at present it's impossible
-#         to put this in aurora/dsdb/4.4.3 as we'd like to.
-
-#TODO: failover support
+        #TODO: failover support
         self.region = (os.environ.get('SYS_REGION') or None)
         if self.region == 'eu':
             self.dsn = 'LNP_DSDB11'
@@ -59,30 +55,8 @@ class DsdbConnection(object):
             self.dsn = 'NYP_DSDB11'
         assert self.dsn
 
-        #FIXME: same as above: T/O restrictions
-        krbusrs = 'daqscott, samsh'
-        #krbusrs = self.cfg.get('dsdb','krbusers').split(',')
-        krbusrs = krbusrs.split(',')
-        assert krbusrs
-
-        self.fake = fake
-
-        if os.environ.get('USER') in krbusrs:
-            self._get_krb_cnxn()
-        else:
-            #TODO: pull password from afs
-            self.syb = Sybase.connect(self.dsn, 'dsdb_guest', 'dsdb_guest',
-                                  'dsdb', datetime = 'auto',
-                                  auto_commit = '0')
-        assert self.syb._is_connected
-
-        ###Impostor attrs only used in unit tests for data refresh
-        if self.fake:
-            self._fake_data = {}
-            #self.fake_types = self.cfg.get('DEFAULT','fake_types')
-            #TODO: cfg file in DSDB (T/O restrictions)
-            self.fake_types = 'country, campus, city, building, network'
-            assert self.fake_types
+        self.syb = Sybase.connect(self.dsn, 'dsdb_guest', 'dsdb_guest',
+                                  'dsdb', auto_commit = '0')
 
     def run_query(self,sql):
         """Runs query sql. Note use runSybaseProc to call a stored procedure.
@@ -114,70 +88,19 @@ class DsdbConnection(object):
             #we're not raising this b/c it means you have
             #to import Sybase everywhere... reconsider this
 
-    #TODO: check for kw 'bldgs' as type list for networks by bldg
-    #TODO: rename oper (operator? not quite. more like data_type)
     def dump(self, data_type, *args, **kw):
-        """ wraps the real 'dump' method for testing.
+        sql = dt[data_type]
 
-            add 'fake' to args, and method = 'removed', 'added', 'updated'
-            example:
-                db.dump('country','fake', method='removed') """
+        if data_type == 'buildings_by_campus':
+            campus=kw.pop('campus')
+            if not campus:
+                raise ValueError('buildings_by_campus requires campus kwarg')
+            sql += "'%s'"% (campus)
 
-        if data_type not in dt.keys():
-            raise KeyError('%s is not a valid dump operation'% data_type)
-
-        if self.fake:
-            if data_type not in self.fake_types:
-                raise ValueError('%s not available for impostor use'% data_type)
-            else:
-                #cache the data between tests
-                if data_type not in self._fake_data.keys():
-                    self._fake_data[data_type] = FakeData(data_type,
-                                                          self._dump(data_type))
-
-            return getattr(self._fake_data[data_type], kw['method'])
-        elif data_type == 'buildings_by_campus':
-            if kw.has_key('campus'):
-                campus = kw.pop('campus')
-            else:
-                msg = "Campus is a required Argument for 'buildings_by_campus'"
-            sql = dt[data_type]
-            sql += "'%s'"%(campus)
-            return self.run_query(sql).fetchall()
-
-        else:
-            #do the regular dump routine
-            return self.run_query(dt[data_type]).fetchall()
-            #this is sorta useless at the moment...
-            return self._dump(dt[data_type])
-
-    def _dump(self, data_type, *args, **kw):
         try:
-            return self.run_query(dt[data_type]).fetchall()
+            return self.run_query(sql).fetchall()
         except Exception,e:
             print e
-
-    #TODO: when Borg, MEMOIZE
-    def _get_principal(self):
-        for line in open('/ms/dist/syb/dba/files/sgp.dat').xreadlines():
-            svr, principal = line.split()
-            if svr == self.dsn:
-                return principal
-
-        raise ValueError('no principal found for %s'%(self.dsn))
-
-    def _get_krb_cnxn(self):
-        #regional copies don't accept kerberos login
-        self.dsn = 'NYP_DSDB11'
-        principal = self._get_principal()
-
-        self.syb = Sybase.connect(self.dsn, '', '', 'dsdb',
-                                      delay_connect=1, datetime='auto')
-
-        self.syb.set_property(Sybase.CS_SEC_NETWORKAUTH, Sybase.CS_TRUE)
-        self.syb.set_property(Sybase.CS_SEC_SERVERPRINCIPAL,
-                              self._get_principal())
-        self.syb.connect()
 
     def get_network_by_sysloc(self,loc):
         """ append a sysloc to the base query, get networks"""
@@ -204,27 +127,6 @@ class DsdbConnection(object):
     def close(self):
         self.syb.close()
 
-class FakeData(object):
-    """ holds a set of data and a few handy manipulations for testing
-        pretend the 'real' data set is the current set - its last item """
-
-    def __init__(self, datatype, data, *args, **kw):
-        if type(data) != list:
-            raise ValueError('data must be a list')
-
-        #deep copy the data list as we'll be modifying it
-        self._last = data.pop()
-        self.removed = copy(data)
-
-        self._modified_info  = 'AQUILON '+self._last[1]
-        self._upd = (self._last[0], self._modified_info, self._last[2])
-
-        self.added = copy(data)
-        self.added.append(self._upd)
-
-        self.updated = copy(data)
-        self.updated.append(self._last)
-
 if __name__ == '__main__':
     from pprint import pprint
 
@@ -233,13 +135,12 @@ if __name__ == '__main__':
 
     print db.get_network_by_sysloc('np.ny.na')
 
-    ops = ('net_type','campus','campus_entries','country','city','building',
-           'np_network')  #omitting 'bucket','network_full','net_ids'
+    ops = ('country','city') #a few ones for testing
 
     for i in ops:
         print 'Dump(%s) from dsdb:\n%s\n'%(i, db.dump(i))
 
-    host = 'blackcomb'
+    host = 'cwipm1'
     print "getting host data for '%s'"%(host)
     print 'host %s is in the "%s" pod'%(host,db.get_host_pod(host))
 
@@ -247,12 +148,6 @@ if __name__ == '__main__':
     pprint(db.dump('buildings_by_campus', campus='ny'), indent =4)
     print ' '
 
-    #del(db) #BORG anyway dude...
-
-    #db = DsdbConnection(fake=True)
-    #for m in ('removed', 'added', 'updated'):
-    #    pprint(db.dump('country', method=m), indent=4)
-    #    print
 
     db.close()
 
