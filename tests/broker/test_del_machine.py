12b9305e52ec19638a6e664fb5bc1a015cd3dcf5
Wesley Hertlein <wesleyhe@ms.com>
2009-06-25 16:08:04 -0400

    del_machine: Added FIXME notes for non-cluster related tests.

--- tests/broker/test_del_machine.py
+++ tests/broker/test_del_machine.py
@@ -123,6 +123,10 @@ class TestDelMachine(TestBrokerCommand):
             command = "show machine --machine ut10s04p%d" % port
             self.notfoundtest(command.split(" "))
 
+    # FIXME: Add a test for deleting an aurora node.  (Might need to happen
+    # in a constraint test for timing reasons.)
+    # FIXME: Add a test for deleting a machine with only auxiliaries.
+
 
 if __name__=='__main__':
     import aquilon.aqdb.depends
