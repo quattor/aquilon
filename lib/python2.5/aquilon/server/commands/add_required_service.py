03b5784d5003b486eb5a3d81f5eddeb47ea67903
Wesley Hertlein <wesleyhe@ms.com>
2009-08-10 16:16:13 -0400

    add_required_service: Fix annoying sql constraint error when adding dupe.

--- lib/python2.5/aquilon/server/commands/add_required_service.py
+++ lib/python2.5/aquilon/server/commands/add_required_service.py
@@ -29,6 +29,7 @@
 """Contains the logic for `aq add required service`."""
 
 
+from aquilon.exceptions_ import ArgumentError
 from aquilon.server.broker import BrokerCommand
 from aquilon.aqdb.model import ServiceListItem
 from aquilon.server.dbwrappers.archetype import get_archetype
@@ -42,6 +43,10 @@ class CommandAddRequiredService(BrokerCommand):
     def render(self, session, service, archetype, comments, **arguments):
         dbarchetype = get_archetype(session, archetype)
         dbservice = get_service(session, service)
+        if ServiceListItem.get_unique(session, archetype_id=dbarchetype.id,
+                                      service_id=dbservice.id):
+            raise ArgumentError("Archetype %s already requires service %s" %
+                                (dbarchetype.name, dbservice.name))
         dbsli = ServiceListItem(archetype=dbarchetype, service=dbservice,
                 comments=comments)
         session.add(dbsli)
