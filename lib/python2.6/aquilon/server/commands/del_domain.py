b89cb5ed91058c63576c4d0b6ddb28e20ef4bd0c
Wesley Hertlein <wesleyhe@ms.com>
2010-02-07 23:16:49 -0500

    del_domain: Refactor to use common code.

--- lib/python2.6/aquilon/server/commands/del_domain.py
+++ lib/python2.6/aquilon/server/commands/del_domain.py
@@ -29,14 +29,9 @@
 """Contains the logic for `aq del domain`."""
 
 
-import os
-
-from aquilon.exceptions_ import ArgumentError
-from aquilon.server.broker import BrokerCommand
-from aquilon.server.processes import remove_dir
-from aquilon.server.locks import lock_queue, CompileKey
-from aquilon.server.templates.domain import TemplateDomain
 from aquilon.aqdb.model import Domain
+from aquilon.server.broker import BrokerCommand
+from aquilon.server.dbwrappers.branch import remove_branch
 
 
 class CommandDelDomain(BrokerCommand):
@@ -44,25 +39,6 @@ class CommandDelDomain(BrokerCommand):
     required_parameters = ["domain"]
 
     def render(self, session, logger, domain, **arguments):
-        # FIXME: This will fail if the domain does not exist.  We might
-        # want to allow the directory to be deleted anyway, assuming it
-        # is a valid domain name and az_check passes.
         dbdomain = Domain.get_unique(session, domain, compel=True)
-        session.refresh(dbdomain)
-        if dbdomain.hosts:
-            raise ArgumentError("Cannot delete domain %s while hosts are still attached."
-                    % dbdomain.name)
-        session.delete(dbdomain)
-
-        # Technically we shouldn't need a lock here at all, since
-        # nothing else should be using this domain.
-        domain = TemplateDomain(dbdomain, logger=logger)
-        key = CompileKey(domain=dbdomain.name, logger=logger)
-        try:
-            lock_queue.acquire(key)
-            for dir in domain.directories():
-                remove_dir(dir, logger=logger)
-        finally:
-            lock_queue.release(key)
-
+        remove_branch(self.config, logger, dbdomain)
         return
