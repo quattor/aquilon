4029759a67824bb424a32f116163b53e5812814e
Wesley Hertlein <wesleyhe@ms.com>
2010-02-28 20:52:58 -0500

    New scale test for changing the build status of a rack.

---
+++ tests/scale/transition_rack.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2.6
+# ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
+#
+# Copyright (C) 2008,2009  Contributor
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the EU DataGrid Software License.  You should
+# have received a copy of the license with this program, and the
+# license is published at
+# http://eu-datagrid.web.cern.ch/eu-datagrid/license.html.
+#
+# THE FOLLOWING DISCLAIMER APPLIES TO ALL SOFTWARE CODE AND OTHER
+# MATERIALS CONTRIBUTED IN CONNECTION WITH THIS PROGRAM.
+#
+# THIS SOFTWARE IS LICENSED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE AND ANY WARRANTY OF NON-INFRINGEMENT, ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THIS
+# SOFTWARE MAY BE REDISTRIBUTED TO OTHERS ONLY BY EFFECTIVELY USING
+# THIS OR ANOTHER EQUIVALENT DISCLAIMER AS WELL AS ANY OTHER LICENSE
+# TERMS THAT MAY APPLY.
+"""Add dummy information for a rack of machines on a /26."""
+
+
+from common import AQRunner, TestRack
+
+
+def transition_rack(building, rackid, status, aqservice):
+    aq = AQRunner(aqservice=aqservice)
+    rack = TestRack(building, rackid)
+    rc = aq.wait(["ping"])
+    for half in [0, 1]:
+        for offset in range(1, 49):
+            host = rack.get_host(half, offset)
+            print "Transitioning host %s to status %s" % (host, status)
+            rc = aq.wait(["reconfigure", "--hostname", host,
+                          "--buildstatus", status])
+
+
+if __name__=='__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-b", "--building", dest="building", type="string",
+                      help="The building name to use")
+    parser.add_option("-r", "--rack", dest="rack", type="int",
+                      help="The rack id to use, 0-7")
+    parser.add_option("-s", "--status", dest="status", type="string",
+                      help="The status to use [build, ready, failed]")
+    parser.add_option("-a", "--aqservice", dest="aqservice", type="string",
+                      help="The service name to use when connecting to aqd")
+    (options, args) = parser.parse_args()
+    if not options.building:
+        parser.error("Missing option --building")
+    if options.rack is None:
+        parser.error("Missing option --rack")
+    if options.rack not in range(8):
+        parser.error("--rack must be 0-7")
+    if options.status is None:
+        parser.error("Missing option --status")
+
+    transition_rack(options.building, options.rack, options.status,
+                    options.aqservice)
