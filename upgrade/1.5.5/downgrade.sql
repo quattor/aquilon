93c5d32cd6dc5aa0dc3f4ba7648c757b6be73d80
Nick Williams <Nick.Williams@morganstanley.com>
2010-08-20 14:47:27 +0100

    changes based on testing
    tested both upgrade and downgrade scripts.

--- upgrade/1.5.5/downgrade.sql
+++ upgrade/1.5.5/downgrade.sql
@@ -1,15 +1,15 @@
 
+alter table host drop constraint host_lifecycle_fk;
 drop table hostlifecycle;
-drop sequence hostlifecycle_id_seq;
-
 rename hostlifecycle_id_seq to status_id_seq;
 
-alter table "host" drop constraint "host_lifecycle_fk";
-alter table "host" rename column lifecycle_id to status_id;
-alter table "host" add constraint host_status_fk foreign key (status_id) references status(id);
+alter table host rename column lifecycle_id to status_id;
+alter table host add constraint host_status_fk foreign key (status_id) references status(id);
 
-alter table "esx_cluster" drop constraint "cluster_lifecycle_fk";
-alter table "esx_cluster" add constraint "cluster_status_fk" foreign key(status_id) references status(id);
+alter table clstr drop constraint cluster_status_fk;
+alter table clstr add constraint cluster_status_fk foreign key(status_id) references status(id);
+alter table clstr modify status_id integer null;
+alter table clstr drop column status_id;
 drop table clusterlifecycle;
 drop sequence clusterlifecycle_id_seq;
 
--- upgrade/1.5.5/upgrade.sql
+++ upgrade/1.5.5/upgrade.sql
@@ -1,4 +1,4 @@
-alter table "host" drop constraint "host_status_fk";
+alter table host drop constraint host_status_fk;
 
 insert into status(id, name, creation_date) values(status_id_seq.nextval, 'decommissioned', sysdate);
 insert into status(id, name, creation_date) values(status_id_seq.nextval, 'rebuild', sysdate);
@@ -8,14 +8,12 @@ rename status_id_seq to hostlifecycle_id_seq;
 create table hostlifecycle (
         id integer not null, 
         name varchar(32) not null, 
-        creation_date datetime not null, 
+        creation_date date not null, 
         comments varchar(255), 
         constraint hostlifecycle_pk primary key (id), 
         constraint hostlifecycle_uk unique (name)
 );
-insert into hostlifecycle (id, name, creation_date, comments) values(
-  select id, name, creation_date, comments from status
-);
+insert into hostlifecycle select * from status;
 
 alter table host rename column status_id to lifecycle_id;
 alter table host add constraint host_lifecycle_fk foreign key (lifecycle_id) references hostlifecycle(id);
@@ -23,17 +21,14 @@ alter table host add constraint host_lifecycle_fk foreign key (lifecycle_id) ref
 create table clusterlifecycle (
         id integer not null, 
         name varchar(32) not null, 
-        creation_date datetime not null, 
+        creation_date date not null, 
         comments varchar(255), 
         constraint clusterlifecycle_pk primary key (id), 
         constraint clusterlifecycle_uk unique (name)
 );
-insert into clusterlifecycle (id, name, creation_date, comments) values(
- select id, name, creation_date, comments from hostlifecycle where
-  name in ('ready', 'build', 'decommissioned', 'rebuild')
-);
-create sequence clusterlifecycle_id_seq start with 20 increment by 1 nocache nocycle;
-alter table "clstr" drop constraint "cluster_status_fk";
-alter table "clstr" add constraint cluster_status_fk foreign key (status_id) references clusterlifecycle(id)
+insert into clusterlifecycle select * from hostlifecycle where name in ('ready', 'build', 'decommissioned', 'rebuild');
+create sequence clusterlifecycle_id_seq start with 50 increment by 1 nocache nocycle;
+alter table clstr add status_id integer default(1) not null;
+alter table clstr add constraint cluster_status_fk foreign key (status_id) references clusterlifecycle(id);
 
 
