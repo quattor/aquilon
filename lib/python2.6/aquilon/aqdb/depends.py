297e97414853b20bdcbd7e0ac0f36c3a7421d5b3
Scott D'Aquila <daqscott@ms.com>
2010-10-21 13:47:59 +0100

    aqdb.depends + aqd.conf.defaults adjustments
    * bump to oracle 11.2.0.1.0
    * bump modulecmd to 1.0.4
    * remove extraneous blank lines from depends
    
    Reviewed-By: Wesley Hertlein <Wesley.Hertlein@morganstanley.com>

--- lib/python2.6/aquilon/aqdb/depends.py
+++ lib/python2.6/aquilon/aqdb/depends.py
@@ -31,13 +31,8 @@
 import ms.version
 
 ms.version.addpkg('sqlalchemy', '0.6.4')
-
-ms.version.addpkg('cx_Oracle', '5.0.1-11.1.0.6')
-
+ms.version.addpkg('cx_Oracle', '5.0.4-11.2.0.1.0')
 ms.version.addpkg('ipython', '0.10')
-
 ms.version.addpkg('argparse', '1.1')
-
-ms.version.addpkg('ms.modulecmd', '1.0.2')
-
+ms.version.addpkg('ms.modulecmd', '1.0.4')
 ms.version.addpkg('ipaddr', '2.0.0')
