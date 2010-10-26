230bcafd89d3830b0b5a00868e3fcd779dae4315
Scott D'Aquila <daqscott@ms.com>
2010-10-26 13:34:26 +0100

    bump to sqlalchemy 0.6.5
    Reviewed-By: Wesley Hertlein <Wesley.Hertlein@morganstanley.com>

--- lib/python2.6/aquilon/aqdb/depends.py
+++ lib/python2.6/aquilon/aqdb/depends.py
@@ -30,14 +30,9 @@
 
 import ms.version
 
-ms.version.addpkg('sqlalchemy', '0.6.4')
-
+ms.version.addpkg('sqlalchemy', '0.6.5')
 ms.version.addpkg('cx_Oracle', '5.0.1-11.1.0.6')
-
 ms.version.addpkg('ipython', '0.10')
-
 ms.version.addpkg('argparse', '1.1')
-
 ms.version.addpkg('ms.modulecmd', '1.0.2')
-
 ms.version.addpkg('ipaddr', '2.1.4')
