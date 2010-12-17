564a0fe31702ae116b99493fd500b89bf684d2a9
Gabor Gombas <gombgabo@ms.com>
2010-12-17 09:58:20 +0100

    Update copyright information
    There are a lot of changes for old files, but I checked a couple and
    those looked correct.

--- lib/python2.6/aquilon/aqdb/model/room.py
+++ lib/python2.6/aquilon/aqdb/model/room.py
@@ -1,6 +1,6 @@
 # ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
 #
-# Copyright (C) 2008,2009,2010  Contributor
+# Copyright (C) 2009,2010  Contributor
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the EU DataGrid Software License.  You should
