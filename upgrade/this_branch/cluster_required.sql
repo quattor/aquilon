fb0d429889bd0aae8429c1276093c80f30f71012
Gabor Gombas <gombgabo@ms.com>
2011-01-24 15:12:30 +0100

    "NOT NULL" should not be a "CHECK" constraint
    Also fix white space so reduce the differences between the upgraded and
    a newly created schema.
    
    Reviewed-By: Scott DAquila <daqscott@ms.com>

--- upgrade/this_branch/cluster_required.sql
+++ upgrade/this_branch/cluster_required.sql
@@ -2,7 +2,6 @@ ALTER TABLE archetype ADD cluster_required NUMBER(*,0);
 UPDATE archetype SET cluster_required = 0;
 UPDATE archetype SET cluster_required = 1 WHERE name = 'vmhost';
 ALTER TABLE archetype ADD CONSTRAINT "ARCHETYPE_CLUSTER_REQUIRED_CK"
-    CHECK (cluster_required IN (0,1)) ENABLE;
-ALTER TABLE archetype ADD CONSTRAINT "ARCHETYPE_CLUSTER_REQUIRED_NN"
-    CHECK (cluster_required IS NOT NULL) ENABLE;
+    CHECK (cluster_required IN (0, 1)) ENABLE;
+ALTER TABLE archetype MODIFY (cluster_required CONSTRAINT "ARCHETYPE_CLUSTER_REQUIRED_NN" NOT NULL ENABLE);
 COMMIT;
