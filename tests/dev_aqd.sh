a6ca94761eb5654ca329eafdad6c8c793b896dc0
Gabor Gombas <gombgabo@ms.com>
2011-04-13 15:52:44 +0200

    Allow running dev_aqd.sh from the top level directory
    Reviewed-By: Wesley Hertlein <Wesley.Hertlein@morganstanley.com>

--- tests/dev_aqd.sh
+++ tests/dev_aqd.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+STARTDIR=`dirname $0`
+
 if [ -z "$AQDCONF" ] ; then
-	AQDCONF=../etc/aqd.conf.dev
+	AQDCONF="$STARTDIR/../etc/aqd.conf.dev"
 	export AQDCONF
 fi
 
-BASEDIR=`../lib/python2.6/aquilon/config.py | grep -A 1000 "\[broker\]" | grep "basedir=" | head -1 | cut -c9-`
+BASEDIR=`$STARTDIR/../lib/python2.6/aquilon/config.py | grep -A 1000 "\[broker\]" | grep "basedir=" | head -1 | cut -c9-`
 
 if [ -z "$BASEDIR" ]; then
 	echo "Failed to determine the base directory" >&2
 	exit 1
 fi
 
-RUNDIR=`../lib/python2.6/aquilon/config.py | grep -A 1000 "\[broker\]" | grep "rundir=" | head -1 | cut -c8-`
-DSN=`../lib/python2.6/aquilon/config.py | grep -A 1000 "\[database\]" | grep "dsn=" | head -1 | cut -c5-`
+RUNDIR=`$STARTDIR/../lib/python2.6/aquilon/config.py | grep -A 1000 "\[broker\]" | grep "rundir=" | head -1 | cut -c8-`
+DSN=`$STARTDIR/../lib/python2.6/aquilon/config.py | grep -A 1000 "\[database\]" | grep "dsn=" | head -1 | cut -c5-`
 
 echo
 echo "Using AQDCONF = $AQDCONF"
@@ -22,5 +24,5 @@ echo "  and rundir  = $RUNDIR"
 echo "  and dsn     = $DSN"
 echo
 
-exec python2.6 ../bin/twistd.py -no -l - --pidfile=$RUNDIR/aqd.pid aqd --config=$AQDCONF "$@"
+exec python2.6 "$STARTDIR/../bin/twistd.py" -no -l - --pidfile=$RUNDIR/aqd.pid aqd --config=$AQDCONF "$@"
 
