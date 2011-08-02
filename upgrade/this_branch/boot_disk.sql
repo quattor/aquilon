5e64cf28d341483ce6178061ac8c8a29fe0adcd8
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2011-08-02 17:25:10 +0200

    Fix the schema upgrade script
    Reviewed-By: Wesley Hertlein <Wesley.Hertlein@morganstanley.com>

--- upgrade/this_branch/boot_disk.sql
+++ upgrade/this_branch/boot_disk.sql
@@ -1,6 +1,7 @@
-ALTER TABLE disk ADD bootable INTEGER DEFAULT 0;
+ALTER TABLE disk ADD bootable INTEGER;
 UPDATE disk SET bootable = 1 WHERE device_name = 'sda' OR device_name = 'c0d0';
+UPDATE disk SET bootable = 0 WHERE bootable IS NULL;
 ALTER TABLE disk MODIFY (bootable INTEGER CONSTRAINT "DISK_BOOTABLE_NN" NOT NULL);
-ALTER TABLE disk ADD CONSTRAINT "DISK_BOOTABLE_CK" CHECK (bootable in (0, 1));
+ALTER TABLE disk ADD CONSTRAINT "DISK_BOOTABLE_CK" CHECK (bootable IN (0, 1));
 
 QUIT;
