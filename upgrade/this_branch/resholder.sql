38d3b7f547f3b810d44e1c8b0f9b2f592386ee77
Wesley Hertlein <Wesley.Hertlein@morganstanley.com>
2011-12-02 14:06:59 -0500

    resholder: Quick tweak to the upgrade script.
    SQLalchemy actually sets up the foreign key relationship to the parent
    table (resource).  Give or take it's the same thing, so adjusting the
    upgrade to match what we get on a fresh build.

--- upgrade/this_branch/resholder.sql
+++ upgrade/this_branch/resholder.sql
@@ -1,5 +1,5 @@
 ALTER TABLE resholder ADD resourcegroup_id INTEGER;
 ALTER TABLE resholder ADD CONSTRAINT resholder_bundle_fk 
-      FOREIGN KEY(resourcegroup_id) REFERENCES resourcegroup (id) 
+      FOREIGN KEY(resourcegroup_id) REFERENCES "resource" (id)
       ON DELETE CASCADE;
 COMMIT;
