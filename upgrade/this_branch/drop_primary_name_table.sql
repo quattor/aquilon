a72fa452034acfa791867ea2408d57e686d420ab
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2012-01-19 12:54:31 +0100

    Add missing foreign key

--- upgrade/this_branch/drop_primary_name_table.sql
+++ upgrade/this_branch/drop_primary_name_table.sql
@@ -1,6 +1,7 @@
-ALTER TABLE hardware_entity ADD primary_name_id NUMBER;
+ALTER TABLE hardware_entity ADD primary_name_id INTEGER;
 UPDATE hardware_entity SET primary_name_id =
 	(SELECT dns_record_id
 		FROM primary_name_association
 		WHERE primary_name_association.hardware_entity_id = hardware_entity.id);
 DROP TABLE primary_name_association;
+ALTER TABLE hardware_entity ADD CONSTRAINT "HW_ENT_PRI_NAME_FK" FOREIGN KEY (primary_name_id) REFERENCES dns_record (id);
