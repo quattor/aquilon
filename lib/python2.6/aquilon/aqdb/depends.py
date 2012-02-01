0b18be1983ebc3572b5e79a8cc0f8423ada359e2
Priya Joshi <priyaj@ms.com>
2012-02-01 11:39:54 -0500

    New version of Sqlalchemy library
    Description: Updgrade sqlalchemy library from 0.7.3 to 0.7.5
    
    Test Plan: all tests run okay
    
    Reviewed-By: Zoltan Sebestyen <Zoltan.Sebestyen@morganstanley.com>

--- lib/python2.6/aquilon/aqdb/depends.py
+++ lib/python2.6/aquilon/aqdb/depends.py
@@ -30,7 +30,7 @@
 
 import ms.version
 
-ms.version.addpkg('sqlalchemy', '0.7.3')
+ms.version.addpkg('sqlalchemy', '0.7.5')
 ms.version.addpkg('cx_Oracle', '5.1-11.2.0.1.0')
 ms.version.addpkg('ipython', '0.10.1')
 ms.version.addpkg('argparse', '1.1')
