f93f7aac8fe2598d5b96d6e9f477877965f69337
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2012-02-16 15:38:31 +0100

    Enhance the primary IP update logic
    Reviewed-By: Wesley Hertlein <Wesley.Hertlein@morganstanley.com>

--- lib/python2.6/aquilon/worker/dbwrappers/hardware_entity.py
+++ lib/python2.6/aquilon/worker/dbwrappers/hardware_entity.py
@@ -38,6 +38,7 @@ from aquilon.aqdb.model.network import get_net_id_from_ip
 from aquilon.worker.dbwrappers.location import get_location
 from aquilon.worker.dbwrappers.interface import (check_ip_restrictions,
                                                  assign_address)
+from aquilon.utils import first_of
 
 
 def search_hardware_entity_query(session, hardware_type=HardwareEntity,
@@ -167,27 +168,49 @@ def convert_primary_name_to_arecord(session, dbhw_ent, ip, dbnetwork):
     dbhw_ent.primary_name = dbdns_rec
 
 def update_primary_ip(session, dbhw_ent, ip):
-    old_ip = dbhw_ent.primary_ip
     dbnetwork = get_net_id_from_ip(session, ip)
-    # Hmm... should this check apply to the switch's own network?
     check_ip_restrictions(dbnetwork, ip)
+
+    # The primary address must be unique
+    q = session.query(AddressAssignment)
+    q = q.filter_by(network=dbnetwork)
+    q = q.filter_by(ip=ip)
+    addr = q.first()
+    if addr:
+        raise ArgumentError("IP address {0} is already in use by {1:l}."
+                            .format(ip, addr.interface))
 
     # Convert ReservedName to ARecord if needed
     if isinstance(dbhw_ent.primary_name, ReservedName):
         convert_primary_name_to_arecord(session, dbhw_ent, ip, dbnetwork)
-    else:
-        dbhw_ent.primary_name.ip = ip
-        dbhw_ent.primary_name.network = dbnetwork
 
-    q = session.query(AddressAssignment)
-    q = q.filter_by(network=dbnetwork)
-    q = q.filter_by(ip=old_ip)
-    q = q.join(Interface)
-    q = q.filter_by(hardware_entity=dbhw_ent)
-    addr = q.first()
-    if addr:
-        addr.ip = ip
+        # When converting a ReservedName to an ARecord, we have to bind the
+        # primary address to an interface. Try to pick one.
+        dbinterface = first_of(dbhw_ent.interfaces, lambda x: x.bootable)
+        if not dbinterface:
+            dbinterface = first_of(dbhw_ent.interfaces, lambda x:
+                                   x.interface_type != "management")
+
+        if not dbinterface:  # pragma: no cover
+            raise AquilonError("Cannot update the primary IP address of {0:l} "
+                               "because it does not have any interfaces "
+                               "defined.".format(dbhw_ent))
+
+        assign_address(dbinterface, ip, dbnetwork)
     else:
-        # This should only happen if the switch did not have an IP
-        # address before
-        assign_address(dbhw_ent.interfaces[0], ip, dbnetwork)
+        dns_rec = dbhw_ent.primary_name
+
+        q = session.query(AddressAssignment)
+        q = q.filter_by(network=dns_rec.network)
+        q = q.filter_by(ip=dns_rec.ip)
+        q = q.join(Interface)
+        q = q.filter_by(hardware_entity=dbhw_ent)
+        # In case of Zebra, the address may be assigned to multiple interfaces
+        addrs = q.all()
+
+        dns_rec.ip = ip
+        dns_rec.network = dbnetwork
+
+        for addr in addrs:
+            addr.ip = ip
+            addr.network = dbnetwork
