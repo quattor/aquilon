af76769f4c84902d787a684e412fdc857c39583d
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2012-08-09 10:59:01 +0200

    Fix node index calculation
    If the cluster was bigger in the past, then existing node indexes may be
    larger than the current cluster size.
    
    Reviewed-By: Wesley Hertlein <Wesley.Hertlein@morganstanley.com>

--- tests/broker/test_rebind_esx_cluster.py
+++ tests/broker/test_rebind_esx_cluster.py
@@ -46,21 +46,40 @@ from brokertest import TestBrokerCommand
 class TestRebindESXCluster(TestBrokerCommand):
 
     # Failure test is in add_virtual_hardware.
-    def testrebindevh1(self):
+    def test_100_rebind_evh1(self):
         self.successtest(["cluster",
                           "--hostname", "evh1.aqd-unittest.ms.com",
                           "--cluster", "utecl2"])
 
-    def testverifyrebindevh1(self):
+    def test_110_unbind_evh2(self):
+        # Let's see if we can put a node back after the cluster size has shrunk
+        command = ["uncluster", "--hostname", "evh2.aqd-unittest.ms.com",
+                   "--personality", "generic", "--cluster", "utecl1"]
+        self.successtest(command)
+
+    def test_111_rebind_evh2(self):
+        command = ["cluster", "--hostname", "evh2.aqd-unittest.ms.com",
+                   "--personality", "vulcan-1g-desktop-prod",
+                   "--cluster", "utecl1"]
+        self.successtest(command)
+
+    def test_200_verifyrebindevh1(self):
         command = "show host --hostname evh1.aqd-unittest.ms.com"
         out = self.commandtest(command.split(" "))
         self.matchoutput(out, "Primary Name: evh1.aqd-unittest.ms.com", command)
         self.matchoutput(out, "Member of ESX Cluster: utecl2", command)
 
-    # FIXME: Also test plenary files.
+        # FIXME: Also test plenary files.
+
+    def test_200_verify_evh2(self):
+        command = ["show", "cluster", "--cluster", "utecl1"]
+        out = self.commandtest(command)
+        self.matchoutput(out,
+                         "Member: evh2.aqd-unittest.ms.com [node_index: 0]",
+                         command)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRebindESXCluster)
     unittest.TextTestRunner(verbosity=2).run(suite)
 
