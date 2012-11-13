9c1366386c1cc0a5ad34d520a31f299d4c0fc503
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2012-11-13 14:43:27 +0100

    Run documentation checks during the testsuite
    The goal is to make sure obvious errors in the documentation are caught
    before a new broker release is made.
    
    Reviewed-By: Nathan.Dimmock@morganstanley.com

---
+++ tests/broker/test_documentation.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2.6
+# ex: set expandtab softtabstop=4 shiftwidth=4: -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
+#
+# Copyright (C) 2012  Contributor
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the EU DataGrid Software License.  You should
+# have received a copy of the license with this program, and the
+# license is published at
+# http://eu-datagrid.web.cern.ch/eu-datagrid/license.html.
+#
+# THE FOLLOWING DISCLAIMER APPLIES TO ALL SOFTWARE CODE AND OTHER
+# MATERIALS CONTRIBUTED IN CONNECTION WITH THIS PROGRAM.
+#
+# THIS SOFTWARE IS LICENSED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE AND ANY WARRANTY OF NON-INFRINGEMENT, ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THIS
+# SOFTWARE MAY BE REDISTRIBUTED TO OTHERS ONLY BY EFFECTIVELY USING
+# THIS OR ANOTHER EQUIVALENT DISCLAIMER AS WELL AS ANY OTHER LICENSE
+# TERMS THAT MAY APPLY.
+"""Module for testing errors in the documentation."""
+
+import os
+import unittest
+from subprocess import Popen, PIPE
+
+if __name__ == '__main__':
+    import utils
+    utils.import_depends()
+
+from brokertest import TestBrokerCommand
+
+
+class TestDocumentation(TestBrokerCommand):
+    def test_100_run_make_check(self):
+        srcdir = self.config.get("broker", "srcdir")
+        docdir = os.path.join(srcdir, "doc")
+        p = Popen(["make", "check"], stdout=PIPE, stderr=PIPE, cwd=docdir)
+        out, err = p.communicate()
+        self.assertEqual(p.returncode, 0,
+                         "Running 'make check' on the documentation failed: "
+                         "STDOUT:\n@@@\n'%s'\n@@@\nSTDERR:\n@@@\n'%s'\n@@@\n"
+                         % (out, err))
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDocumentation)
+    unittest.TextTestRunner(verbosity=2).run(suite)
