7937ab4c15b4a28870008e5620a30bba9592e45e
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2013-06-21 16:19:44 +0200

    Fix incremental audit update
    Change-Id: Id3e4e898c1549cf9aca30fce6c2c9d59dab271fd

--- upgrade/1.10.9/move_audit_tables_merge.sql
+++ upgrade/1.10.9/move_audit_tables_merge.sql
@@ -5,7 +5,7 @@ MERGE /*+ APPEND PARALLEL */ INTO xtn2 USING xtn ON (xtn2.xtn_id = xtn.xtn_id)
 		VALUES (xtn.xtn_id, xtn.username, xtn.command, xtn.is_readonly, xtn.start_time);
 COMMIT;
 
-MERGE /*+ APPEND PARALLEL */ INTO xtn2_detail USING xtn_detail ON (xtn2_detail.xtn_id = xtn_detail.xtn_id)
+MERGE /*+ APPEND PARALLEL */ INTO xtn2_detail USING xtn_detail ON (xtn2_detail.xtn_id = xtn_detail.xtn_id AND xtn2_detail.name = xtn_detail.name AND xtn2_detail.value = xtn_detail.value)
 	WHEN NOT MATCHED THEN INSERT (xtn_id, name, value)
 		VALUES (xtn_detail.xtn_id, xtn_detail.name, xtn_detail.value);
 COMMIT;
