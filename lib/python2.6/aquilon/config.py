a0a2bc91ef698cc6faba64996afd3d8ea4c7c206
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-09-09 15:12:00 +0200

    Use more the configuration_directory function

--- lib/python2.6/aquilon/config.py
+++ lib/python2.6/aquilon/config.py
@@ -23,7 +23,7 @@ import pwd
 from ConfigParser import SafeConfigParser
 
 from aquilon.exceptions_ import AquilonError
-
+from aquilon.utils import configuration_directory
 
 def get_username():
     return pwd.getpwuid(os.getuid()).pw_name
@@ -38,8 +38,10 @@ global_defaults = {
             "user": os.environ.get("USER") or get_username(),
             # Only used by unit tests at the moment, but maybe useful for
             # scripts that want to execute stand-alone.
-            "srcdir": os.path.realpath(os.path.join(
-                            os.path.dirname(__file__), "..", "..", "..")),
+            "srcdir": os.path.realpath(configuration_directory(
+                                       os.path.join(
+                                           os.path.dirname(__file__),
+                                           "..", "..", ".."))),
             "hostname": socket.getfqdn(),
         }
 
