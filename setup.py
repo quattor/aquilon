654cf69ae19964ae4ff778e00a2ecf431587419d
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-09-09 17:23:31 +0200

    Bug fix

--- setup.py
+++ setup.py
@@ -78,6 +78,6 @@ setup(name="aquilon",
       data_files=[("/usr/share/aquilon/etc",
                    glob.glob("etc/*.conf*") + glob.glob("etc/*xml")),
                   ("/etc/init.d", ["etc/rc.d/init.d/aqd"]),
-                  ("/etc/sysconfig", "etc/sysconfig/aqd")],
+                  ("/etc/sysconfig", ["etc/sysconfig/aqd"])],
       scripts=all_scripts,
       url="http://quattor.org")
