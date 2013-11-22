5aca11abc2b7507401ced8de1197c7fda48ccef9
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2013-11-22 18:09:44 +0100

    Update copyright messages
    Change-Id: I31b3cd1e3c862c35ee1b8766b05316f2b7a40e7e

--- build/build_schema_htdocs.py
+++ build/build_schema_htdocs.py
@@ -2,7 +2,7 @@
 # -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
 # ex: set expandtab softtabstop=4 shiftwidth=4:
 #
-# Copyright (C) 2008,2009,2010,2011,2012,2013  Contributor
+# Copyright (C) 2013  Contributor
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 """
     This module produces some HTML docuemation of the the database model.
     It does so by inspecting the database module to produce a set of
