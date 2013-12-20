2e3753fc52a20c469c8a861a2524df3a56779925
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-12-20 16:05:22 +0100

    Package the man pages
    Thanks to @stdweird for the hints on how to handle the man pages on SL6

--- build/cleanup-ms.sh
+++ build/cleanup-ms.sh
@@ -1,4 +1,9 @@
 %setup -n %{name}-%{unmangled_version}
 grep -lrE '(import aquilon.*depends)|(from aquilon.*import depends)' . \
     |xargs sed -i '/\(import aquilon.*depends\)\|\(from aquilon.*import depends\)/d'
+stylesheets=$(echo /usr/share/sgml/docbook/xsl*stylesheets-*)
 sed -i '/ms.version/d' bootstrap/gen_completion.py
+sed -i "s:/ms/dist/fsf/PROJ/docbook-xsl-ns/.*/common:$stylesheets:" \
+    doc/style-man.xsl
+sed -i "s:/ms/dist/fsf/PROJ/docbook-xsl-ns/.*/common:$stylesheets:" \
+    doc/style-html.xsl
