03f4013ad50ee8dcc17532d1612e5e5b59444180
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-12-23 15:17:19 +0100

    More fixes for the man pages

--- build/cleanup-ms.sh
+++ build/cleanup-ms.sh
@@ -1,7 +1,7 @@
 %setup -n %{name}-%{unmangled_version}
 grep -lrE '(import aquilon.*depends)|(from aquilon.*import depends)' . \
     |xargs sed -i '/\(import aquilon.*depends\)\|\(from aquilon.*import depends\)/d'
-stylesheets=$(echo /usr/share/sgml/docbook/xsl*stylesheets-*)
+stylesheets=$(echo /usr/share/sgml/docbook/xsl-stylesheets-*)
 sed -i '/ms.version/d' bootstrap/gen_completion.py
 sed -i "s:/ms/dist/fsf/PROJ/docbook-xsl-ns/.*/common:$stylesheets:" \
     doc/style-man.xsl
