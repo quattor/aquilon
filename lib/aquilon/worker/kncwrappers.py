3b31eca57b5922bdebbc5a2d86460e8faaa73006
Dave Reeve <Dave.Reeve@morganstanley.com>
2014-02-26 11:36:58 +0000

    Refactoring KNCHTTPChannel and adding some error handeling
    Change-Id: I2b34337880ffce6536a4791df3739a79126c175b
    Reviewed-by: Gabor Gombas <Gabor.Gombas@morganstanley.com>

--- lib/aquilon/worker/kncwrappers.py
+++ lib/aquilon/worker/kncwrappers.py
@@ -16,8 +16,16 @@
 # limitations under the License.
 """Wrappers for using knc with the stock twisted server implementations."""
 
-
 from twisted.web import server, http
+
+import logging
+from aquilon.utils import force_ascii, force_ipv4
+
+LOGGER = logging.getLogger(__name__)
+
+
+class KNCProtocolException(Exception):
+    pass
 
 
 class KNCHTTPChannel(http.HTTPChannel):
@@ -27,34 +35,60 @@ class KNCHTTPChannel(http.HTTPChannel):
     about the connection.  This needs to be stripped
     (and saved), which is all this does.
     """
+    __KNC_fields = {
+        'CREDS': force_ascii,
+        'REMOTE_IP': force_ipv4
+    }
 
     def __init__(self, *args, **kwargs):
-        self.__needKNCInfo = 1
+        self.__need_knc_data = 1
+        self.logger = LOGGER
         self.kncinfo = {}
         http.HTTPChannel.__init__(self, *args, **kwargs)
 
-    # FIXME: Should probably throw some sort of error if the line
-    # does not follow the expected format.
-    # This is odd since http is using \r\n but knc is just using \n...
-    # We essentially get all the knc info *and* the first line of client
-    # info in the "first line".
+    def kncLineReceived(self, data):
+        # KNC uses '\n' to delimit lines, while HTTP uses '\n\r', therefore
+        # all of the KNC data comes on the first line.  Split these up
+        # and process them speratly.
+        lines = data.split('\n')
+        for line in lines:
+            if not self.__need_knc_data:
+                # Pass remaining data through the the HTTP Channel
+                http.HTTPChannel.lineReceived(self, line)
+            elif line == 'END':
+                # Check that we have now recieved all of the metadata
+                # that we are expecting...
+                for field, checker in self.__KNC_fields.iteritems():
+                    if field not in self.kncinfo:
+                        raise KNCProtocolException('Missing %s' % field)
+                self.__need_knc_data = 0
+            else:
+                if not line:
+                    raise KNCProtocolException('Malformed KNC request')
+                if ':' not in line:
+                    raise KNCProtocolException('Malformed KNC metatdata')
+                (key, value) = line.split(':', 1)
+                if not key:
+                    raise KNCProtocolException('KNC Metadata key missing')
+                if not value:
+                    raise KNCProtocolException('KNC Metadata value missing')
+                if key in self.__KNC_fields:
+                    try:
+                        self.kncinfo[key] = self.__KNC_fields[key](key, value)
+                    except ArgumentError, e:
+                        raise KNCProtocolException(e.message)
+
     def lineReceived(self, line):
-        if self.__needKNCInfo:
+        if self.__need_knc_data:
             self.resetTimeout()
-            lines = line.split("\n")
-            if len(lines) > 1:
-                for l in lines:
-                    self.lineReceived(l)
-                return
-            if line == 'END':
-                self.__needKNCInfo = 0
-                #log.msg("Finished receiving knc info.")
-                return
-            (key, value) = line.split(':', 1)
-            #log.msg("Got knc info key='%s' value='%s'" % (key, value))
-            self.kncinfo[key] = value
-            return
-        http.HTTPChannel.lineReceived(self, line)
+            try:
+                self.kncLineReceived(line)
+            except KNCProtocolException, e:
+                self.logger.warning("Closed KNC Connection: %s" % e.message)
+                self.transport.write("HTTP/1.1 400 Bad KNC Request\r\n\r\n")
+                self.transport.loseConnection()
+        else:
+            http.HTTPChannel.lineReceived(self, line)
 
     def getPrincipal(self):
         return self.kncinfo.get("CREDS")
