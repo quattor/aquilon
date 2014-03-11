3d3ad0669eb6e4621431fc0f2efde3db0824b3cc
Dave Reeve <Dave.Reeve@morganstanley.com>
2014-03-11 11:35:29 +0000

    Refactoring host creation into a dbwrapper
    This commit moved the generic logic for creating a host object
    into a dbwapper.  This will allow it to be reused by the network
    device creation.
    
    Change-Id: Ia11511df230a7f7e5e2a80611437801271e8a625
    Reviewed-by: Gabor Gombas <Gabor.Gombas@morganstanley.com>

--- lib/aquilon/worker/dbwrappers/user_principal.py
+++ lib/aquilon/worker/dbwrappers/user_principal.py
@@ -24,7 +24,6 @@ from sqlalchemy.orm import contains_eager, joinedload
 
 from aquilon.exceptions_ import ArgumentError, InternalError, NotFoundException
 from aquilon.aqdb.model import Role, Realm, UserPrincipal
-from aquilon.worker.dbwrappers.host import hostname_to_host
 
 
 LOGGER = logging.getLogger(__name__)
@@ -48,6 +47,7 @@ def get_or_create_user_principal(session, principal, createuser=True,
     if m:
         user = 'aquilonhost'
         # Verify that the host exists in AQDB
+        from aquilon.worker.dbwrappers.host import hostname_to_host
         hostname_to_host(session, m.group(1))
 
     # Short circuit the common case, and optimize it to eager load in
