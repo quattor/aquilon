79023985a7f499d9e3603ab7c2d5a7e67fa20795
Luis Fernando Muñoz Mejías <lfmunozmejias@gmail.com>
2014-03-19 16:36:46 +0000

    Look up default files in /usr/share/aquilon/etc
    The RPM installs them there.

--- lib/aquilon/config.py
+++ lib/aquilon/config.py
@@ -42,7 +42,7 @@ def config_filename(name):
     if os.path.exists(os.path.join(_SRCDIR, "etc", "aqd.conf.dev")):
         return os.path.join(_SRCDIR, "etc", name)
 
-    paths_to_try = [os.path.join("/etc", name),
+    paths_to_try = [os.path.join("/etc", "aquilon", name),
                     os.path.join("/usr", "share", "aquilon", "etc", name),
                     os.path.join(_SRCDIR, "etc", name)]
     for path in paths_to_try:
