66552256e03cc99882899262e8bc69f1bbd7547f
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2014-04-18 13:35:01 +0200

    Update copyright notices
    Change-Id: If68c651712364e804db00069224e766c81b18d04

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
+# -*- cpy-indent-level: 4; indent-tabs-mode: nil -*-
+# ex: set expandtab softtabstop=4 shiftwidth=4:
 #
+# Copyright (C) 2013  Contributor
 #
-# This file is part of Aquilon.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# See the LICENSE file for all the licensing information.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Basic setup.py for packaging Aquilon itself"""
 
 import glob
