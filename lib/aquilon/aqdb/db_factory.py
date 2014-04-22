quattor/aquilon

commit a5e6df651d639444159ea5c9956ca74e5536082d

DbFactory cleanups
Make the pool settings optional, and move them out from the general
[database] section to better reflect the fact that SQLite does not use
these settings. Add support for setting pool_recycle since it's trivial
and someone may find it useful.

Move event registration out of the login() method to reduce the number
of places we care about the exact DB backend. Register the events for
the Pool class, to make sure they apply to the very first DB connection.

Integrate _get_password_list() into login(), since that was the only
caller, and this way we can avoid some redundant error checks.

Drop the dsn and meta attributes because they're redundant. Use
SQLAlchemy utilities for parsing the dsn.

Change-Id: Ia0e948f1d1487dad431e488b84ef52fb0cd5d037

etc/aqd.conf.defaults
@@ -49,26 +49,6 @@ dbuser =
 dsn = oracle://%(dbuser)s:PASSWORD@%(server)s
 module = orcl/client/11.2.0.1.0
 
-[database_sqlite]
-dbfile = %(quattordir)s/aquilondb/aquilon.db
-dsn = sqlite:///%(dbfile)s
-
-[database_postgresql]
-# Leave the server name empty for unix socket on localhost
-server =
-dbuser = %(user)s
-dbname =
-#password_file =
-dsn = postgresql+psycopg2://%(dbuser)s:PASSWORD@%(server)s/%(dbname)s
-# Schema the xtn_log tables exist in, if separate from the rest of the tables.
-# There is no default - the value is only used if set.
-#audit_schema = %(user)
-
-[database]
-# Unfortunately, the code hack for this means any database defaults
-# needed as config variables must be in the general DEFAULT section.
-database_section = database_sqlite
-# The pool values do not currently apply to sqlite.
 # There are two pools - one for commands that use locks and one for
 # commands that don't.  These values apply to each pool.
 # The pool size needs to take into account both the number of
@@ -82,8 +62,44 @@ database_section = database_sqlite
 # as needed for a database connection from the pool.
 pool_size = 3
 pool_max_overflow = 0
-pool_timeout =
+#pool_timeout =
+#pool_recycle =
 
+[database_sqlite]
+dbfile = %(quattordir)s/aquilondb/aquilon.db
+dsn = sqlite:///%(dbfile)s
+# SQLite does not use connection pooling, so the pool_* options won't work
+# here.
+
+[database_postgresql]
+# Leave the server name empty for unix socket on localhost
+server =
+dbuser = %(user)s
+dbname =
+#password_file =
+dsn = postgresql+psycopg2://%(dbuser)s:PASSWORD@%(server)s/%(dbname)s
+# Schema the xtn_log tables exist in, if separate from the rest of the tables.
+# There is no default - the value is only used if set.
+#audit_schema = %(user)
+
+# There are two pools - one for commands that use locks and one for
+# commands that don't.  These values apply to each pool.
+# The pool size needs to take into account both the number of
+# active connections the database can handle and the number of
+# active commands the broker can handle in memory.  While the broker
+# is running as 32-bit we're deliberately leaving the pool size low.
+# With the pool size so low having overflow connections is # useless.
+# They would be continually active and just increasing load by opening
+# and closing connections.
+# Having timeout unset allows for incoming connections to wait as long
+# as needed for a database connection from the pool.
+pool_size = 3
+pool_max_overflow = 0
+#pool_timeout =
+#pool_recycle =
+
+[database]
+database_section = database_sqlite
 
 [broker]
 default_organization = ms

lib/aquilon/aqdb/db_factory.py
@@ -22,28 +22,30 @@ import sys
 import logging
 from numbers import Number
 
-from aquilon.aqdb import depends
+from aquilon.aqdb import depends  # pylint: disable=W0611
 from aquilon.config import Config
 from aquilon.utils import monkeypatch
 from aquilon.exceptions_ import AquilonError
 
 from sqlalchemy.exc import DatabaseError
-from sqlalchemy import MetaData, create_engine, text, event
+from sqlalchemy import create_engine, text, event
+from sqlalchemy.engine.url import make_url
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.pool import Pool
 from sqlalchemy.schema import CreateIndex, Sequence
 from sqlalchemy.dialects.oracle.base import OracleDDLCompiler
 
 
 # Add support for Oracle-specific index extensions
 @compiles(CreateIndex, 'oracle')
-def visit_create_index(create, compiler, **kw):
+def visit_create_index(create, compiler, **kw):  # pylint: disable=W0613
     index = create.element
     compiler._verify_index_table(index)
     preparer = compiler.preparer
 
-    text = "CREATE "
+    text = "CREATE "  # pylint: disable=W0621
     if index.unique:
         text += "UNIQUE "
     if index.kwargs.get("oracle_bitmap", False):
@@ -67,8 +69,8 @@ def visit_create_index(create, compiler, **kw):
 
 # Add support for table compression
 @monkeypatch(OracleDDLCompiler)
-def post_create_table(self, table):
-    text = ""
+def post_create_table(self, table):  # pylint: disable=W0613
+    text = ""  # pylint: disable=W0621
     compress = table.kwargs.get("oracle_compress", False)
     if compress:
         if isinstance(compress, basestring):
@@ -79,22 +81,22 @@ def post_create_table(self, table):
     return text
 
 
-def sqlite_foreign_keys(dbapi_con, con_record):
+def sqlite_foreign_keys(dbapi_con, con_record):  # pylint: disable=W0613
     dbapi_con.execute('pragma foreign_keys=ON')
 
 
-def sqlite_no_fsync(dbapi_con, con_record):
+def sqlite_no_fsync(dbapi_con, con_record):  # pylint: disable=W0613
     dbapi_con.execute('pragma synchronous=OFF')
 
 
-def oracle_set_module(dbapi_con, con_record):
+def oracle_set_module(dbapi_con, con_record):  # pylint: disable=W0613
     # Store the program's name in v$session. Trying to set a value longer than
     # the allowed length will generate ORA-24960, so do an explicit truncation.
     prog = os.path.basename(sys.argv[0])
     dbapi_con.module = prog[:48]
 
 
-def oracle_reset_action(dbapi_con, con_record):
+def oracle_reset_action(dbapi_con, con_record):  # pylint: disable=W0613
     # Reset action and clientinfo in v$session. The DB connection may be closed
     # when this function is called, so be careful.
     if dbapi_con is None:
@@ -132,53 +134,45 @@ class DbFactory(object):
             except ModulecmdExecError, err:
                 log.error("Failed to load module %s: %s" % (module, err))
 
-        self.dsn = config.get('database', 'dsn')
+        pool_options = {}
 
-        self.pool_options = {}
-        self.pool_options["pool_size"] = config.getint("database", "pool_size")
-        self.pool_options["max_overflow"] = config.getint("database",
-                                                          "pool_max_overflow")
-        if len(config.get("database", "pool_timeout").strip()) > 0:
-            self.pool_options["pool_timeout"] = config.getint("database",
-                                                              "pool_timeout")
-        else:
-            self.pool_options["pool_timeout"] = None
-        log.info("Database engine using pool options %s" % self.pool_options)
+        for optname in ("pool_size", "pool_timeout", "pool_recycle"):
+            if config.has_option("database", optname):
+                pool_options[optname] = config.getint("database", optname)
+        # Sigh. max_overflow does not start with pool_*
+        if config.has_option("database", "pool_max_overflow"):
+            pool_options["max_overflow"] = config.getint("database",
+                                                         "pool_max_overflow")
+        log.info("Database engine using pool options %s" % pool_options)
 
-        passwds = self._get_password_list(config)
+        dsn = config.get('database', 'dsn')
+        dialect = make_url(dsn).get_dialect()
 
-        # ORACLE
-        if self.dsn.startswith('oracle'):
-            import cx_Oracle  # pylint: disable=W0612
-
-            self.login(passwds)
-
-        # POSTGRESQL
-        elif self.dsn.startswith('postgresql'):
-            import psycopg2  # pylint: disable=W0612
-
-            self.login(passwds)
-
-        # SQLITE
-        elif self.dsn.startswith('sqlite'):
-            self.engine = create_engine(self.dsn)
-            self.no_lock_engine = None
-            event.listen(self.engine, "connect", sqlite_foreign_keys)
+        if dialect.name == "oracle":
+            # Events should be registered before we try to open a real
+            # connection below, because the underlying DBAPI connection will not
+            # be closed
+            event.listen(Pool, "connect", oracle_set_module)
+            event.listen(Pool, "checkin", oracle_reset_action)
+            self.login(config, dsn, pool_options)
+        elif dialect.name == "postgresql":
+            self.login(config, dsn, pool_options)
+        elif dialect.name == "sqlite":
+            event.listen(Pool, "connect", sqlite_foreign_keys)
             if config.has_option("database", "disable_fsync") and \
                config.getboolean("database", "disable_fsync"):
-                event.listen(self.engine, "connect", sqlite_no_fsync)
+                event.listen(Pool, "connect", sqlite_no_fsync)
                 log = logging.getLogger(__name__)
                 log.info("SQLite is operating in unsafe mode!")
+
+            self.engine = create_engine(dsn)
+            self.no_lock_engine = None
             connection = self.engine.connect()
             connection.close()
         else:
-            msg = "Supported database datasources are postgresql, oracle and sqlite.\n"
-            msg += "yours is '%s' " % self.dsn
-            sys.stderr.write(msg)
-            sys.exit(9)
-
-        self.meta = MetaData(self.engine)
-        assert self.meta
+            raise AquilonError("Supported database datasources are postgresql, "
+                               "oracle and sqlite. You've asked for: %s" %
+                               dialect.name)
 
         self.Session = scoped_session(sessionmaker(bind=self.engine))
         assert self.Session
@@ -192,29 +186,26 @@ class DbFactory(object):
         else:
             self.NLSession = self.Session
 
-    def login(self, passwds):
-        errs = []
+    def login(self, config, raw_dsn, pool_options):
+        # Default: no password
+        passwords = [""]
         pswd_re = re.compile('PASSWORD')
-        dsn_copy = self.dsn
 
-        if not passwds:
-            raise AquilonError("At least one password must be specified, even "
-                               "if that's empty.")
+        if config.has_option("database", "password_file"):
+            passwd_file = config.get("database", "password_file")
+            if passwd_file:
+                with open(passwd_file) as f:
+                    passwords = [line.strip() for line in f.readlines()]
 
-        for p in passwds:
-            self.dsn = re.sub(pswd_re, p, dsn_copy)
-            self.engine = create_engine(self.dsn, **self.pool_options)
-            self.no_lock_engine = create_engine(self.dsn, **self.pool_options)
+                if not passwords:
+                    raise AquilonError("Password file %s is empty." %
+                                       passwd_file)
 
-            # Events should be registered before we try to open a real
-            # connection below, because the underlying DBAPI connection will not
-            # be closed
-            if self.engine.dialect.name == "oracle":
-                event.listen(self.engine, "connect", oracle_set_module)
-                event.listen(self.no_lock_engine, "connect", oracle_set_module)
-                event.listen(self.engine, "checkin", oracle_reset_action)
-                event.listen(self.no_lock_engine, "checkin",
-                             oracle_reset_action)
+        errs = []
+        for p in passwords:
+            dsn = re.sub(pswd_re, p, raw_dsn)
+            self.engine = create_engine(dsn, **pool_options)
+            self.no_lock_engine = create_engine(dsn, **pool_options)
 
             try:
                 connection = self.engine.connect()
@@ -226,31 +217,7 @@ class DbFactory(object):
         if errs:
             raise errs.pop()
         else:
-            raise AquilonError('Failed to connect to %s')
-
-    def _get_password_list(self, config):
-        """
-        Read a password file containing one password per line. The passwords
-        will be tried in the order they appear in the file.
-        """
-
-        # Default: no password
-        if not config.has_option("database", "password_file") or \
-           not config.get("database", "password_file").strip():
-            return [""]
-
-        passwd_file = config.get("database", "password_file")
-        if not os.path.exists(passwd_file):
-            raise AquilonError("The password file '%s' does not exist." %
-                               passwd_file)
-
-        passwds = ""
-        with open(passwd_file) as f:
-            passwds = f.readlines()
-            if not passwds:
-                raise AquilonError("Password file %s is empty." % passwd_file)
-
-        return [passwd.strip() for passwd in passwds]
+            raise AquilonError('Failed to connect to %s' % raw_dsn)
 
     def get_sequences(self):
         """ return a list of the sequence names from the current databases

build/build_schema_htdocs.py
@@ -161,7 +161,7 @@ db = DbFactory()
 Base.metadata.bind = db.engine
 orm.configure_mappers()
 Base.metadata.create_all()
-db.meta.reflect()
+Base.metadata.reflect()
 
 
 def table_coverage_check():
@@ -175,7 +175,7 @@ def table_coverage_check():
         for cls in model_group[group]['classes']:
             tbl = cls.__tablename__
             seen[tbl] = 1
-    for tbl in db.meta.tables.keys():
+    for tbl in Base.metadata.tables.keys():
         if tbl in seen:
             del seen[tbl]
         else:
@@ -185,8 +185,8 @@ def table_coverage_check():
 
 
 def write_schema_group_png(group):
-    tables = list((db.meta.tables[cls.__tablename__]
-                for cls in model_group[group]['classes']))
+    tables = list((Base.metadata.tables[cls.__tablename__]
+                   for cls in model_group[group]['classes']))
     pngfile = os.path.join(opts.dir, "%s.%s.png" % (opts.prefix, group))
     schema2dot.create_schema_graph(tables=tables).write_png(pngfile)
 

build/graph_schema.py
@@ -52,6 +52,6 @@ ms.modulecmd.load("fsf/graphviz/2.28.0")
 from aquilon.aqdb.utils import schema2dot
 
 
-dot = schema2dot.create_schema_graph(metadata=db.meta)
+dot = schema2dot.create_schema_graph(metadata=Base.metadata)
 dot.write(os.path.join(opts.dir, "%s.dot" % opts.prefix))
 dot.write_png(os.path.join(opts.dir, "%s.png" % opts.prefix))

tests/aqdb/add_admin.py
@@ -85,7 +85,7 @@ def main(*args, **kw):
     Base.metadata.bind = db.engine
 
     if opts.verbose:
-        db.meta.bind.echo = True
+        db.engine.echo = True
 
     session = db.Session()
 

tests/aqdb/build_db.py
@@ -90,7 +90,7 @@ def main(*args, **kw):
     Base.metadata.bind = db.engine
 
     if opts.verbose:
-        db.meta.bind.echo = True
+        db.engine.echo = True
 
     if opts.delete_db == True:
         log.debug('Dropping database')

commit 2b4cbbbfd6ce77fb68cc2b2e203624ecc09ef232

Fix DB pool timeout setting
Commit a5e6df651d639444159ea5c9956ca74e5536082d accidentally removed the
ability to pass pool_timeout=None to the DB engine, which causes
breakage if there are long running commands and no available DB
connections.

Change-Id: I6d23a004a4a74aebc77732bdf58594ef3a4a4ec6
Addresses-Issue: Jira/AQUILON-1863
Reviewed-by: Dave Reeve <Dave.Reeve@morganstanley.com>

etc/aqd.conf.defaults
@@ -51,18 +51,20 @@ module = orcl/client/11.2.0.1.0
 
 # There are two pools - one for commands that use locks and one for
 # commands that don't.  These values apply to each pool.
+#
 # The pool size needs to take into account both the number of
 # active connections the database can handle and the number of
 # active commands the broker can handle in memory.  While the broker
 # is running as 32-bit we're deliberately leaving the pool size low.
-# With the pool size so low having overflow connections is # useless.
+# With the pool size so low having overflow connections is useless.
 # They would be continually active and just increasing load by opening
 # and closing connections.
-# Having timeout unset allows for incoming connections to wait as long
-# as needed for a database connection from the pool.
+#
+# Having timeout defined but unset allows for incoming connections to wait as
+# long as needed for a database connection from the pool.
 pool_size = 3
 pool_max_overflow = 0
-#pool_timeout =
+pool_timeout =
 #pool_recycle =
 
 [database_sqlite]
@@ -84,18 +86,20 @@ dsn = postgresql+psycopg2://%(dbuser)s:PASSWORD@%(server)s/%(dbname)s
 
 # There are two pools - one for commands that use locks and one for
 # commands that don't.  These values apply to each pool.
+#
 # The pool size needs to take into account both the number of
 # active connections the database can handle and the number of
 # active commands the broker can handle in memory.  While the broker
 # is running as 32-bit we're deliberately leaving the pool size low.
-# With the pool size so low having overflow connections is # useless.
+# With the pool size so low having overflow connections is useless.
 # They would be continually active and just increasing load by opening
 # and closing connections.
-# Having timeout unset allows for incoming connections to wait as long
-# as needed for a database connection from the pool.
+#
+# Having timeout defined but unset allows for incoming connections to wait as
+# long as needed for a database connection from the pool.
 pool_size = 3
 pool_max_overflow = 0
-#pool_timeout =
+pool_timeout =
 #pool_recycle =
 
 [database]

lib/aquilon/aqdb/db_factory.py
@@ -138,7 +138,12 @@ class DbFactory(object):
 
         for optname in ("pool_size", "pool_timeout", "pool_recycle"):
             if config.has_option("database", optname):
-                pool_options[optname] = config.getint("database", optname)
+                if len(config.get("database", optname).strip()) > 0:
+                    pool_options[optname] = config.getint("database", optname)
+                else:
+                    # pool_timeout can be set to None
+                    pool_options[optname] = None
+
         # Sigh. max_overflow does not start with pool_*
         if config.has_option("database", "pool_max_overflow"):
             pool_options["max_overflow"] = config.getint("database",