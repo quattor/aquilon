eb81d5330677b443bfeb543033778979a1a1a01f
Priya Joshi <priyaj@ms.com>
2014-09-23 13:57:04 -0400

    License upgrade and move sql to release upgrade scripts.

--- upgrade/1.11.36/feature_owner.sql
+++ upgrade/1.11.36/feature_owner.sql
@@ -7,7 +7,7 @@ ALTER TABLE feature ADD owner_eon_id INTEGER;
 ALTER TABLE feature ADD visibility VARCHAR(16);
 ALTER TABLE feature ADD CONSTRAINT feature_owner_grn_fk FOREIGN KEY (owner_eon_id) REFERENCES grn (eon_id);
 UPDATE feature SET owner_eon_id = 6980;
-UPDATE feature set visibility = 'public'; 
+UPDATE feature set visibility = 'public';
 ALTER TABLE feature MODIFY (owner_eon_id CONSTRAINT feature_owner_eon_id_nn NOT NULL);
 ALTER TABLE feature MODIFY (visibility CONSTRAINT feature_visibility_nn NOT NULL);
 QUIT;
