a85d8fe86a5ca45e2b1b5650dce6f51e541852f2
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2014-10-28 13:08:26 +0100

    Clean up the parameter protobuf formatter
    The protobuf formatter for parameters expects a different input than the
    raw formatter. Move that logic from the formatter class into the
    command, to make the behavior of the formatter match the other
    formatters.
    
    Change-Id: I1987be8e04eae3e2814aa2b834cff1517b6f13d2
    Reviewed-by: Priya Joshi <Priya.Joshi@morganstanley.com>

--- lib/aquilon/worker/commands/show_parameter.py
+++ lib/aquilon/worker/commands/show_parameter.py
@@ -16,15 +16,16 @@
 # limitations under the License.
 
 from aquilon.exceptions_ import NotFoundException
-from aquilon.aqdb.model import Personality
+from aquilon.aqdb.model import Personality, Parameter
 from aquilon.worker.broker import BrokerCommand
+from aquilon.worker.formats.parameter import PersonalityProtoParameter
 
 
 class CommandShowParameterPersonality(BrokerCommand):
 
     required_parameters = ['personality']
 
-    def render(self, session, personality, archetype, **arguments):
+    def render(self, session, personality, archetype, style, **arguments):
         dbpersonality = Personality.get_unique(session, name=personality,
                                                archetype=archetype, compel=True)
         if not dbpersonality.paramholder or \
@@ -32,4 +33,35 @@ class CommandShowParameterPersonality(BrokerCommand):
             raise NotFoundException("No parameters found for {0:l}."
                                     .format(dbpersonality))
 
-        return dbpersonality.paramholder.parameters
+        # Unfortunately, the raw and the protobuf formatters operate on
+        # different data: the protobuf formatter groups the values per parameter
+        # definition, while the raw formatter groups them by the top-level key
+        # (which is usually the template name).
+        if style == 'proto':
+            params = PersonalityProtoParameter()
+
+            param_definitions = None
+            paramdef_holder = dbpersonality.archetype.paramdef_holder
+
+            for param in dbpersonality.paramholder.parameters:
+                if paramdef_holder:
+                    param_definitions = paramdef_holder.param_definitions
+                    for param_def in param_definitions:
+                        value = param.get_path(param_def.path, compel=False)
+                        if value:
+                            params.append((param_def.path, param_def, value))
+
+                for link in dbpersonality.features:
+                    if not link.feature.paramdef_holder:
+                        continue
+                    param_definitions = link.feature.paramdef_holder.param_definitions
+                    for param_def in param_definitions:
+                        value = param.get_feature_path(link, param_def.path,
+                                                       compel=False)
+                        if value:
+                            path = Parameter.feature_path(link, param_def.path)
+                            params.append((path, param_def, value))
+
+            return params
+        else:
+            return dbpersonality.paramholder.parameters
