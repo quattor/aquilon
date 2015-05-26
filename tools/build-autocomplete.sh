01fd914f2077d1b23a0d716a58ea54eb0872b245
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2015-05-26 16:06:45 +0200

    Update copyright notices
    Change-Id: Ifa47ad4faae1b2f90e39fbafca9bd8bc8900f7c5

--- tools/build-autocomplete.sh
+++ tools/build-autocomplete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2013,2014  Contributor
+# Copyright (C) 2013,2014,2015  Contributor
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
