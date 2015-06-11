6a252dc9ec892534440313a26b37dcbe1085ffce
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2015-06-11 12:46:13 +0200

    Fix constraint name after the column renaming
    Change-Id: I0dea2f3016cd5dfc956fbb1c5eafc21b2017a059

--- upgrade/this_branch/application.sql
+++ upgrade/this_branch/application.sql
@@ -1,4 +1,5 @@
 ALTER TABLE application RENAME COLUMN eonid TO eon_id;
+ALTER TABLE application RENAME CONSTRAINT application_eonid_nn TO application_eon_id_nn;
 ALTER TABLE application ADD CONSTRAINT application_grn_fk FOREIGN KEY (eon_id) REFERENCES grn (eon_id);
 
 QUIT;
