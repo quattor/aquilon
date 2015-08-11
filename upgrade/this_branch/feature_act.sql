3d48d32c628aa831c92f342ec8cec5785c0eb885
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2015-08-11 13:59:48 +0200

    Schema fixes and PostgreSQL upgrade scripts
    Change-Id: I579ba12d2c471105d89a6b47dc1a9901dd2e4c88

--- upgrade/this_branch/feature_act.sql
+++ upgrade/this_branch/feature_act.sql
@@ -1,10 +1,8 @@
-- Disable autocommit, abort if something goes wrong
+-- Disable autocommit, abort if something goes wrong
 SET autocommit off;
 -- WHENEVER sqlerror EXIT FAILURE ROLLBACK;
 SET serveroutput on;
 
-ALTER TABLE feature ADD activation VARCHAR(10);
-ALTER TABLE feature ADD deactivation VARCHAR(10);
+ALTER TABLE feature ADD activation VARCHAR2(10 CHAR);
+ALTER TABLE feature ADD deactivation VARCHAR2(10 CHAR);
 QUIT;
-~
-
--- upgrade/this_branch/parameter_definition.sql
+++ upgrade/this_branch/parameter_definition.sql
@@ -1,10 +1,8 @@
-- Disable autocommit, abort if something goes wrong
+-- Disable autocommit, abort if something goes wrong
 SET autocommit off;
 -- WHENEVER sqlerror EXIT FAILURE ROLLBACK;
 SET serveroutput on;
 
-ALTER TABLE param_definition ADD activation VARCHAR(10);
-ALTER TABLE param_definition DROP rebuild_required;
+ALTER TABLE param_definition ADD activation VARCHAR2(10 CHAR);
+ALTER TABLE param_definition DROP COLUMN rebuild_required;
 QUIT;
-~
-
