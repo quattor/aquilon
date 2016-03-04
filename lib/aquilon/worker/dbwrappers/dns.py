dfee08bf49de96a03ecd906abbe0545846e93198
Dave Reeve <Dave.Reeve@morganstanley.com>
2016-03-04 15:17:06 +0000

    Revert "Optimize FQDN cleanup"
    In order to detect deleted objects within a session we need to
    ensure the the objects are loaded.  In this case this means that
    we need to revert this earlier optiomization.
    
    This reverts commit a33c02edaccb260489aacb14df9861baa5f606ba.
    
    Change-Id: I0ee6ad52f825c08af4d95f8b517598ea713410d2
    Reviewed-by: Gabor Gombas <Gabor.Gombas@morganstanley.com>

--- lib/aquilon/worker/dbwrappers/dns.py
+++ lib/aquilon/worker/dbwrappers/dns.py
@@ -20,7 +20,7 @@ import socket
 
 from sqlalchemy.orm import object_session, joinedload, lazyload
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.sql import or_, and_
+from sqlalchemy.sql import or_
 
 from aquilon.exceptions_ import ArgumentError, AquilonError, NotFoundException
 from aquilon.aqdb.model import (Fqdn, DnsDomain, DnsRecord, ARecord,
@@ -98,11 +98,12 @@ def delete_dns_record(dbdns_rec, locked=False, verify_assignments=False):
     session.flush()
 
     # Delete the FQDN if it is orphaned
-    q = session.query(Fqdn)
-    q = q.filter(and_(Fqdn.id == dbfqdn.id,
-                      ~Fqdn.dns_records.any()))
-    q = q.delete(synchronize_session=False)
-    session.expunge(dbfqdn)
+    q = session.query(DnsRecord)
+    q = q.filter_by(fqdn=dbfqdn)
+    if q.count() == 0:
+        session.delete(dbfqdn)
+    else:
+        session.expire(dbfqdn, ['dns_records'])
 
     # Delete the orphaned targets
     for tgt in targets:
