baa358c4a97a4a5818d56f345b743701a9b4cba8
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2016-04-05 14:04:27 +0200

    Fix the schema upgrade scripts
    Change-Id: I190d860112aefe27785d668680d94aa8d1403bb2

--- upgrade/this_branch/dns_env_normalization.sql
+++ upgrade/this_branch/dns_env_normalization.sql
@@ -1,4 +1,4 @@
-ALTER TABLE address_assignment DROP dns_environment_id;
-ALTER TABLE router_address DROP dns_environment_id;
+ALTER TABLE address_assignment DROP COLUMN dns_environment_id;
+ALTER TABLE router_address DROP COLUMN dns_environment_id;
 
 QUIT;
