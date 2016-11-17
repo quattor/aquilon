66ff072c211980175209d77a35a0a14bc8d5d50b
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2016-11-17 11:02:50 +0100

    Small UserSync reorg
    Pass PlenaryCollection as parameter. This will help with the upcoming
    transformation of PlenaryCollection instantiation.
    
    Change-Id: Ic60cdd3cbd6ea7731010fe6bc2f60b543a6cae7c
    Reviewed-by: Dave Reeve <Dave.Reeve@morganstanley.com>

--- lib/aquilon/worker/commands/refresh_user.py
+++ lib/aquilon/worker/commands/refresh_user.py
@@ -19,14 +19,16 @@
 from aquilon.worker.broker import BrokerCommand
 from aquilon.worker.dbwrappers.user import UserSync
 from aquilon.worker.locks import SyncKey
+from aquilon.worker.templates import PlenaryCollection
 
 
 class CommandRefreshUser(BrokerCommand):
 
     def render(self, session, logger, incremental, ignore_delete_limit, **_):
+        plenaries = PlenaryCollection(logger=logger)
         with SyncKey(data="user", logger=logger):
-            sync = UserSync(self.config, session, logger, incremental,
-                            ignore_delete_limit)
+            sync = UserSync(self.config, session, logger, plenaries,
+                            incremental, ignore_delete_limit)
             sync.refresh_user()
 
         return
