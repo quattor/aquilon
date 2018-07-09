ce144cd4a91fb7069ab8b988d48c62d43f5ebeff
Eleonore Carpentier <eleonore.carpentier@morganstanley.com>
2018-07-09 11:36:53 -0400

    Add train test -t jenkins
    Change-Id: Ie5c58f722c9130f145156e778ca6663c61a497a1
    Addresses-Issue: Jira/AQUILON-4994
    Reviewed-By: Gintare Urbone <Gintare.Urbone@morganstanley.com>
    Reviewed-By: Raphael Beamonte <Raphael.Beamonte@morganstanley.com>
    Reviewed-By: Fred Barnes <Fred.Barnes@morganstanley.com>

---
+++ tests/jenkinstest.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -x
+#
+# Copyright (C) 2018 Contributor
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TESTDIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+BASEDIR="$(dirname "$(dirname "$TESTDIR")")"
+
+mkdir -p $BASEDIR/run
+
+# Look for a couple of free ports to use
+while true; do
+    KNCPORT=$((RANDOM % 10000 + 10000))
+    OPENPORT=$((KNCPORT + 1))
+    GITPORT=$((KNCPORT + 2))
+    if [ -z "$(/usr/sbin/ss -n -l -t sport = :$KNCPORT or sport = :$OPENPORT or sport = :$GITPORT | tail -n +2)" ]; then
+	break
+    fi
+done
+
+# Massage unittest.conf
+sed     -e "s!^basedir =.*\$!basedir = $BASEDIR/run!" \
+	-e "s/^kncport =.*$/kncport = ${KNCPORT}/" \
+	-e "s/^openport =.*$/openport = ${OPENPORT}/" \
+	-e "s/^git_port =.*$/git_port = ${GITPORT}/" \
+	$TESTDIR/unittest.conf > \
+	$BASEDIR/unittest.conf
+
+# Make sure UNITTEST_FAILURE is returned if tests fail otherwise jenkins does not mark build as failed
+$TESTDIR/runtests.py --config $BASEDIR/unittest.conf --coverage 2>&1 --no-interactive | tee $TESTDIR/aqdtests.log
+if ! grep -q '^OK$' $TESTDIR/aqdtests.log; then
+    echo UNITTEST_FAILURE
+    exit 1
+fi
