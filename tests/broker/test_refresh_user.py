4907f58394411c1865f59e9a87c43745f4b5705c
Raphaël Beamonte <raphael.beamonte@morganstanley.com>
2018-08-15 22:48:12 -0400

    refresh_user: use a source script instead of a source list
    The list of users was provided through a source list
    until now, which meant that the user information could
    not come from anything else than a file, hence requiring
    us to have that file ready when running the command.
    This updates that behaviour by using a source script
    instead; Anyone wanting to still use a file can basically
    'cat' it in a script and feed that script to Aquilon.
    
    The expected input format also changed a bit, as we now
    expect a well-formatted CSV, comma-separated, with a
    header line. The header line will be used to parse the
    CSV. If a 'type' column is provided, it will be used as
    the user type. If no 'type' column is provided, all user
    types will default to the one defined in the configuration.
    
    Change-Id: Ifeb34326dd6f50612eb93c18b4040a8fbabd8423
    Addresses-Issue: Jira/AQUILON-5273
    Tested-by: Aquilon Template Build testing and verification
    Reviewed-by: Cesar A. Sanchez <Cesar.A.Sanchez@morganstanley.com>

--- tests/broker/test_refresh_user.py
+++ tests/broker/test_refresh_user.py
@@ -30,19 +30,6 @@ from brokertest import TestBrokerCommand
 
 
 class TestRefreshUser(TestBrokerCommand):
-    def test_000_patch_userlist(self):
-        dst_filename = self.config.get("broker", "user_list_location")
-        src_filename = dst_filename + ".in"
-        with open(src_filename, "r") as f:
-            lines = f.readlines()
-
-        # Make sure the current user is always there
-        pwrec = pwd.getpwuid(os.getuid())
-        lines.insert(0, "%s\t%s:dontcare:1000:1000:Current user:%s:%s\n" %
-                     (self.user, pwrec[0], pwrec[5], pwrec[6]))
-
-        with open(dst_filename, "w") as f:
-            f.writelines(lines)
 
     def test_110_grant_testuser4_root(self):
         command = ["grant_root_access", "--user", "testuser4",
@@ -60,7 +47,7 @@ class TestRefreshUser(TestBrokerCommand):
         self.matchoutput(out, "testuser4", command)
 
     def test_200_refresh(self):
-        command = ["refresh", "user"]
+        command = ["refresh_user"]
         err = self.statustest(command)
         self.matchoutput(err,
                          "Duplicate UID: 1236 is already used by testuser3, "
@@ -77,6 +64,7 @@ class TestRefreshUser(TestBrokerCommand):
         self.matchclean(out, "testuser4", command)
         self.matchclean(out, "bad_line", command)
         self.matchclean(out, "dup_uid", command)
+        self.matchclean(out, "foo", command)
         self.matchoutput(out, "testbot1", command)
         self.matchoutput(out, "testbot2", command)
 
@@ -104,17 +92,17 @@ class TestRefreshUser(TestBrokerCommand):
         command = ["show_user", "--username", "testbot1"]
         out = self.commandtest(command)
         self.searchoutput(out, r'User: testbot1$', command)
-        self.searchoutput(out, r'Type: human$', command)
+        self.searchoutput(out, r'Type: robot$', command)
         self.searchoutput(out, r'UID: 1337$', command)
         self.searchoutput(out, r'GID: 655$', command)
         self.searchoutput(out, r'Full Name: test bot 1$', command)
         self.searchoutput(out, r'Home Directory: /tmp/bothome1$', command)
 
-    def test_210_verify_testbot2_still_robot(self):
+    def test_210_verify_testbot2_not_robot(self):
         command = ["show_user", "--username", "testbot2"]
         out = self.commandtest(command)
         self.searchoutput(out, r'User: testbot2$', command)
-        self.searchoutput(out, r'Type: robot$', command)
+        self.searchoutput(out, r'Type: human$', command)
         self.searchoutput(out, r'UID: 1338$', command)
         self.searchoutput(out, r'GID: 655$', command)
         self.searchoutput(out, r'Full Name: test bot 2$', command)
@@ -136,6 +124,10 @@ class TestRefreshUser(TestBrokerCommand):
                         "--full_name", "Some other name",
                         "--home_directory", "/tmp"] + self.valid_just_sn)
 
+    def test_300_update_testbot1(self):
+        self.noouttest(["update_user", "--username", "testbot1",
+                        "--type", "human"] + self.valid_just_sn)
+
     def test_300_update_testbot2(self):
         self.noouttest(["update_user", "--username", "testbot2",
                         "--type", "robot"] + self.valid_just_sn)
@@ -150,6 +142,12 @@ class TestRefreshUser(TestBrokerCommand):
         self.searchoutput(out, r'Full Name: Some other name$', command)
         self.searchoutput(out, r'Home Directory: /tmp$', command)
 
+    def test_301_verify_testbot1_before_sync(self):
+        command = ["show_user", "--username", "testbot1"]
+        out = self.commandtest(command)
+        self.searchoutput(out, r'User: testbot1$', command)
+        self.searchoutput(out, r'Type: human$', command)
+
     def test_301_verify_testbot2_before_sync(self):
         command = ["show_user", "--username", "testbot2"]
         out = self.commandtest(command)
@@ -157,17 +155,25 @@ class TestRefreshUser(TestBrokerCommand):
         self.searchoutput(out, r'Type: robot$', command)
 
     def test_305_refresh_again(self):
-        command = ["refresh", "user", "--incremental"]
+        command = ['refresh_user', '--incremental']
         err = self.partialerrortest(command)
         self.matchoutput(err,
-                         "Duplicate UID: 1236 is already used by testuser3, "
-                         "skipping dup_uid.",
+                         'Duplicate UID: 1236 is already used by testuser3, '
+                         'skipping dup_uid.',
                          command)
         self.matchoutput(err,
-                         "Updating human user testuser3 (uid = 1236, was "
-                         "1237; gid = 655, was 123; "
-                         "full_name = test user 3, was Some other name; "
-                         "home_dir = /tmp/foo, was /tmp)",
+                         'Updating human user testuser3 (uid = 1236, was '
+                         '1237; gid = 655, was 123; '
+                         'full_name = test user 3, was Some other name; '
+                         'home_dir = /tmp/foo, was /tmp)',
+                         command)
+        self.matchoutput(err,
+                         'Updating robot user testbot1 (type = robot, was '
+                         'human)',
+                         command)
+        self.matchoutput(err,
+                         'Updating human user testbot2 (type = human, was '
+                         'robot)',
                          command)
 
     def test_310_verify_testuser1_again(self):
@@ -190,11 +196,17 @@ class TestRefreshUser(TestBrokerCommand):
         self.searchoutput(out, r'Full Name: test user 3$', command)
         self.searchoutput(out, r'Home Directory: /tmp/foo$', command)
 
+    def test_310_verify_testbot1_again(self):
+        command = ["show_user", "--username", "testbot1"]
+        out = self.commandtest(command)
+        self.searchoutput(out, r'User: testbot1$', command)
+        self.searchoutput(out, r'Type: robot$', command)
+
     def test_310_verify_testbot2_again(self):
         command = ["show_user", "--username", "testbot2"]
         out = self.commandtest(command)
         self.searchoutput(out, r'User: testbot2$', command)
-        self.searchoutput(out, r'Type: robot$', command)
+        self.searchoutput(out, r'Type: human$', command)
 
     def test_310_verify_all_again(self):
         command = ["show_user", "--all"]
@@ -219,11 +231,11 @@ class TestRefreshUser(TestBrokerCommand):
 
     def test_321_refresh_refuse(self):
         limit = self.config.getint("broker", "user_delete_limit")
-        command = ["refresh", "user"]
+        command = ["refresh_user"]
         out = self.statustest(command)
         self.matchoutput(out,
                          "Cowardly refusing to delete %s users, because "
-                         "it's over the limit of %s.  Use the "
+                         "it is over the limit of %s.  Use the "
                          "--ignore_delete_limit option to override." %
                          (limit + 5, limit),
                          command)
